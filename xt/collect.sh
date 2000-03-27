208af960e04250f730af6ebfa76334ff16abcc03
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2000-03-27 14:52:41 +0000

    * Added GPL License headers         * use automake/configure to configure/build all components of XT
    svn path=/trunk/StrategoXT/; revision=68

---
+++ xt/collect.sh
@@ -0,0 +1,123 @@
+#! /bin/sh
+# XT -- Program Transformation tools
+# Copyright (C) 2000 Merijn de Jonge <mdejonge@cwi.nl>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id: collect.sh,v 1.1 2000/03/27 15:52:41 mdejonge Exp $
+#
+#
+# This script will collect all required packages for the CT distribution.
+#
+# usage:
+#   collect configure.in pkg-list
+# where
+#   pkg-list is a file containing a list of packages. 
+#
+# Each line in the file `pkg-list' should have the following structure:
+#
+#   <pkg-name>,<pkg-version>,<pkg location>
+#
+# pkg location is an url specifying the location of the package, e.g.:
+#   cvs:/ufs/gipe/CVS   Specifies that the package should be build by first
+#                       checking it out from the repository and then
+#                       executing a "make dist" to build a distribution
+#   http:/www.cw...     Obtain a complete distribution via http
+# 
+#
+# The directory ./pkgs is created to store all software packages
+#
+# The script creates a file SOFTWARE which contains a list of package names,
+# package versions, and package locations.
+#v
+
+
+do_collect () {
+   pkg=$1
+   pkg_version=$2
+   pkg_url=$3
+   
+   case ${pkg_url} in
+      cvs* )
+         echo "Building a distribution for \"${pkg}\" from CVS.">&2
+         cvsroot=`echo ${pkg_url} | sed 's/cvs://g'`
+         cvs -d ${cvsroot} checkout ${pkg}
+         cd ${pkg}
+         ./reconf
+         ./configure
+         gmake dist
+         cp ${pkg}-${pkg_version}.tar.gz ..
+         cd ..
+         rm -fr ${pkg}
+         ;;
+                  
+      http*)
+         echo "Obtaining a distribution of \"${pkg}\" via HTTP." >&2
+         lynx -dump ${pkg_url} > ${pkg}-${pkg_version}.tar.gz ;;
+      * ) "echo unrecognized url: \"$pkg_url\"" >&2; exit 1 ;;
+   esac
+}
+
+if [ $? -ne 2 ]; then
+   echo "usage: $0 configure.in pkg-list" >&2
+   exit 1
+fi
+
+configure=$1
+pkg_file=$2
+pkgs="`grep -v ^# ${pkg_file} | cut -d, -f1`"
+
+# collect and unpack software packages
+if [ ! -d ./pkgs ]; then
+   mkdir -p ./pkgs
+fi
+
+for pkg in ${pkgs}
+do
+   pkg_version=`grep ^${pkg}, ${pkg_file} | cut -d, -f2`
+   pkg_url=`grep ^${pkg}, ${pkg_file} | cut -d, -f3-`
+
+   if [ ! -f ./pkgs/${pkg}-${pkg_version}.tar.gz ]; then
+      cd ./pkgs
+      do_collect ${pkg} ${pkg_version} ${pkg_url}
+
+      cd ..
+   fi
+   if [ ! -d ${pkg}-${pkg_version} ] ; then
+      gunzip -c ./pkgs/${pkg}-${pkg_version}.tar.gz | tar xf -
+      rm -f ./${pkg}
+      ln -s ${pkg}-${pkg_version} ${pkg}
+   fi
+
+done
+
+# Generate text file describing all packages in XT 
+xt_version=`grep AM_INIT ${configure} | tr -d -c [0-9.]`
+
+(
+echo "XT version ${xt_version} is a collection of the following packages:"
+echo
+ 
+for pkg in ${pkgs}
+do
+   pkg_version=`grep ^${pkg}, ${pkg_file} | cut -d, -f2`
+   pkg_url=`grep ^${pkg}, ${pkg_file} | cut -d, -f3-`
+
+   echo "   ${pkg}-${pkg_version} from ${pkg_url}"
+done
+) > SOFTWARE
+
+
