6b2bca1fc057130e4e93ddd3929027b3de958e1e
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2001-06-05 13:40:08 +0000

    fixed copyright notices
    svn path=/trunk/StrategoXT/; revision=344

--- aterm-tools/genconf.sh
+++ aterm-tools/genconf.sh
@@ -1,7 +1,24 @@
 #! /bin/sh
 #
-# $Id: genconf.sh,v 1.3 2001/05/30 16:12:04 mdejonge Exp $
+# Copyright (C) 2001 Merijn de Jonge <mdejonge@cwi.nl>
 #
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
+# $Id: genconf.sh,v 1.4 2001/06/05 14:39:28 mdejonge Exp $
+
 # Author: Merijn de Jonge (mdejonge@cwi.nl)
 #
 # This script generates a configure.in script from a package
