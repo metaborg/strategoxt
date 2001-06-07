ceba7606425c2cfef4c6b598fb3e4d5e9898bcfd
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2001-06-07 11:18:51 +0000

    * src/sdf-imports: imports tool added
    svn path=/trunk/StrategoXT/; revision=384

---
+++ sdf-tools/src/sdf-imports/get-modules.r
@@ -0,0 +1,97 @@
+	\literate[{\btt PACK-MODULES}]
+
+% Copyright (C) 2000 Merijn de Jonge <mdejonge@cwi.nl>
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
+
+% $Id: get-modules.r,v 1.1 2001/06/07 12:18:48 mdejonge Exp $
+
+\begin{code}
+module get-modules
+imports options pack-graph graph-terms Literal-lib GraphXML
+
+signature
+   constructors
+      GraphOutput    : Option
+      GraphXMLOutput : Option
+      FullPathName   : Option
+      Include        : String -> Option
+      Ext            : String -> Option
+   constructors
+      Node : String                           -> GraphItem
+      Node : ModuleName * Location            -> GraphItem
+      Node : ModuleName * Location * OutEdges -> GraphItem
+      Edge : SourceString * TargetString      -> GraphItem
+
+strategies
+
+   get-modules( get-imports, def-ext ) =
+   parse-options( 
+            ArgOption( "-I", \ x -> Include(x) \ ) +
+            ArgOption( "-e", \ x -> Ext(x) \ ) +
+               Option( "-g", !GraphOutput ) +
+               Option( "-l", !FullPathName ) +
+               Option( "--graphxml" , !GraphXMLOutput ) + 
+               io-options );
+   ?options;
+
+   // Option handling
+   try(need-help(get-modules-usage));
+   where( filter( \ Include(p)->p \ ) => path );
+   where(option-defined(?Ext(ext) ) <+ def-ext => ext);
+   where(option-defined(?Input(infile)) <+ stdin => infile );
+
+   <get-imports(!path, !ext)> infile;
+   (
+      where(<option-defined(?FullPathName())>options);
+      map(try(\ Node(_,x) -> Node(x) \ ))
+   <+
+      map(try(\ Node(x,_) -> Node(x) \ ))
+   );
+   (
+      // Generate IG output
+      where(<option-defined(?GraphOutput())>options) ;
+      split( collect( \ Node(x)    ->  <mkterm>("id", [x]) \ ), 
+             collect( \ Edge(x, y) -> [<mkterm>("id", [x]),
+                                       <mkterm>("id", [y])] \ ) ); 
+      ?(nodes, edges); 
+      !graph( nodes(nodes), edges(edges))
+   <+
+      // Generate GraphXML output
+      where(<option-defined(?GraphXMLOutput())>options) ;
+      split( collect( \ Node(x)    -> node(name(<quote>x)) \ ), 
+             collect( \ Edge(x, y) -> edge([source(<quote>x), 
+                                      target(<quote>y)]) \ ) ); 
+      ?(nodes, edges); 
+      !GraphXML( [], [graph([], <conc>(nodes, edges))])
+   <+
+      filter( \Node(x) -> x\ ) 
+   );
+   split( !options, id);
+   output-file'
+
+
+  get-modules-usage =
+    option-defined(?Program(prog));
+    <printnl> (stderr,
+	       ["usage : ", prog, 
+                " [-S] [-I dir] [-i file]",
+		" [-o file] [-s] [--help|-h|-?]",
+                " [-g] [-l]",
+                " [--graphxml]",
+                " [-e ext]" ]);
+    <exit> 1
+\end{code}
---
+++ sdf-tools/src/sdf-imports/sdf-imports.r
@@ -0,0 +1,106 @@
+\literate
+% GT -- Grammar Tools
+% Copyright (C) 2000 Merijn de Jonge <mdejonge@cwi.nl>
+%                    Eelco Visser <visser@acm.org>
+%                    Joost Visser <jvisser@cwi.nl>
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
+
+% $Id: sdf-imports.r,v 1.1 2001/06/07 12:18:48 mdejonge Exp $
+
+\begin{code}
+module imports-sdf
+imports get-modules asfix
+        lib pack-graph pack file Sdf-ParseTree-Syntax Kernel-Sdf-Syntax
+        sglr gt-paths
+signature
+sorts Node
+constructors
+   Node: X* X * X -> Z
+
+strategies
+
+  main = get-modules(imports-sdf, !"sdf")
+
+  imports-sdf(mkpt, ext) =
+	\ root -> (root, ["." | <mkpt>()], []) \;
+	graph-nodes(parse-sdf(ext),
+		    out-edges,
+                    add-node )
+
+
+parse-sdf( ext) =
+        (guarantee-extension(ext), id);
+        ?module-name;
+        find-in-path => m;
+        debug(!"  including ");
+        split(id, \'in -> <sglr> (<concat-strings>[SDFTOOLS, "/share/sdf-tools/sdf.cons.tbl"], 'in)\ ) => tree;
+        <get-sdf-imports>tree => i;
+        <get-module-name>tree => n;
+        !Node(module-name, m, i)
+
+rules
+
+add-node: (m, Node( (f,p), location, edges), xs) -> <conc>(xs',[Node(m, location)])
+   where
+      <map( add_edge(!m) ) >edges => edges';
+      <conc>(edges', xs) => xs'
+
+out-edges:
+   x -> edges
+   where
+      ?Node( _,_, edges )
+
+get-sdf-imports:
+   x -> y
+   where 
+      <collect(?appl(prod(_, cf(sort("Import")), _), _); get-module-name, skip)>x => y
+
+get-module-name:
+   x -> y
+   where
+      <oncetd(?appl(prod(_,cf(sort("ModuleId")),_),_);yield=>y)>x
+
+add_edge(k) : x -> e
+   where
+      k             => k';
+      !Edge(k', x ) => e
+
+strategies
+   yield = rec x(appl(id, map(x)); \ appl(p, ts) -> <concat> ts \
+	        <+ \ a -> [a] \ ); 
+           implode-string 
+
+  skip(x,y) =  parsetree(x,id) 
+             <+
+               appl(prod(id, cf(sort("Module")), id), [id,id,id,x,id,x])
+             <+
+               appl(prod(id, cf(opt(layout) 
+                              <+ 
+                                sort("Productions") 
+                              <+
+                                sort("Symbols") 
+                              <+
+                                sort("Priorities")  
+                              <+
+                                sort("Restrictions")  
+                              <+
+                                sort("Aliases")  
+                               ), id), id ) ; y
+             <+
+                prod(id,id,id); y
+
+\end{code}
