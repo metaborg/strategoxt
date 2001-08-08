be60bc80e37f988ef1fcc04ad72573bf573a5802
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2001-08-08 09:43:11 +0000

    * src/treeviz: Fixed and improved treeviz tool
    svn path=/trunk/StrategoXT/; revision=486

--- aterm-tools/src/treeviz/GraphXML.r
+++ aterm-tools/src/treeviz/GraphXML.r
@@ -1,35 +1,78 @@
-(*
- GT -- Grammar Tools
- Copyright (C) 2000 Merijn de Jonge <mdejonge@cwi.nl>
-                    Eelco Visser <visser@acm.org>
-                    Joost Visser <jvisser@cwi.nl>
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2, or (at your option)
- any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- 02111-1307, USA.
-
- $Id: GraphXML.r,v 1.1 2001/05/30 11:49:20 mdejonge Exp $
-*)
-
-module GraphXML
+module ExternalDTD
   signature
     constructors
-      GraphXML : List(GraphXML-Attribute) * List(Graph) -> GraphXML
-      graph : List(Graph-Attribute) * List(Either(Node,Edge)) -> Graph
-      node : List(Node-Attribute) -> Node
-      edge : List(Edge-Attribute) -> Edge
-      name : CDATA -> Name-Attribute
-      source : CDATA -> Source-Attribute
-      target : CDATA -> Target-Attribute
-      class  : CDATA -> Class-Attribute
+      GraphXML : List(GraphXML-Attribute) * (List(Either(Label,Either(Data,Either(Dataref,Either(Properties,Style))))),List(Graph),List(Either(Edit,Edit-bundle))) -> GraphXML
+      graph : List(Graph-Attribute) * (List(Either(Label,Either(Data,Either(Dataref,Either(Properties,Either(Style,Either(Icon,Size))))))),List(Either(Node,Edge))) -> Graph
+      vendor : AttValue -> Graph-Attribute
+      version : AttValue -> Graph-Attribute
+      Id : AttValue -> Graph-Attribute
+      class : AttValue -> Graph-Attribute
+      preferredlayout : AttValue -> Graph-Attribute
+      icon : List(Icon-Attribute) * EMPTY -> Icon
+      edit : List(Edit-Attribute) * (List(Either(Label,Either(Data,Either(Dataref,Properties)))),List(Either(Node,Edge))) -> Edit
+      action : AttValue -> Edit-Attribute
+      class : AttValue -> Edit-Attribute
+      edit-bundle : List(Edit-bundle-Attribute) * List(Edit) -> Edit-bundle
+      class : AttValue -> Edit-bundle-Attribute
+      node : List(Node-Attribute) * List(Either(Label,Either(Data,Either(Dataref,Either(Properties,Either(Style,Either(Position,Either(Size,Either(Transform,Subgraph-style))))))))) -> Node
+      node1 : List(Node-Attribute) -> Node
+      name : AttValue -> Node-Attribute
+      isMetanode : AttValue -> Node-Attribute
+      class : AttValue -> Node-Attribute
+      edge : List(Edge-Attribute) * List(Either(Label,Either(Data,Either(Dataref,Either(Properties,Either(Style,Path)))))) -> Edge
+      edge1 : List(Edge-Attribute) -> Edge
+      name : AttValue -> Edge-Attribute
+      source : AttValue -> Edge-Attribute
+      target : AttValue -> Edge-Attribute
+      class : AttValue -> Edge-Attribute
+      isDirected : AttValue -> Edge-Attribute
+      properties : List(Properties-Attribute) * EMPTY -> Properties
+      class : AttValue -> Properties-Attribute
+      label : List(Label-Attribute) * PCDATA -> Label
+      class : AttValue -> Label-Attribute
+      data : List(Data-Attribute) * PCDATA -> Data
+      class : AttValue -> Data-Attribute
+      dataref : List(Dataref-Attribute) * List(Ref) -> Dataref
+      class : AttValue -> Dataref-Attribute
+      ref : List(Ref-Attribute) * EMPTY -> Ref
+      class : AttValue -> Ref-Attribute
+      position : List(Position-Attribute) * EMPTY -> Position
+      x : AttValue -> Position-Attribute
+      y : AttValue -> Position-Attribute
+      z : AttValue -> Position-Attribute
+      class : AttValue -> Position-Attribute
+      size : List(Size-Attribute) * EMPTY -> Size
+      width : AttValue -> Size-Attribute
+      height : AttValue -> Size-Attribute
+      depth : AttValue -> Size-Attribute
+      class : AttValue -> Size-Attribute
+      path : List(Path-Attribute) * List(Position) -> Path
+      type : AttValue -> Path-Attribute
+      class : AttValue -> Path-Attribute
+      transform : List(Transform-Attribute) * EMPTY -> Transform
+      matrix : AttValue -> Transform-Attribute
+      class : AttValue -> Transform-Attribute
+      style : List(Style-Attribute) * List(Either(Line,Either(Fill,Implementation))) -> Style
+      subgraph-style : List(Subgraph-style-Attribute) * List(Either(Line,Either(Fill,Implementation))) -> Subgraph-style
+      line : List(Line-Attribute) * EMPTY -> Line
+      tag : AttValue -> Line-Attribute
+      class : AttValue -> Line-Attribute
+      color : AttValue -> Line-Attribute
+      colour : AttValue -> Line-Attribute
+      colour_start : AttValue -> Line-Attribute
+      colour_end : AttValue -> Line-Attribute
+      color_start : AttValue -> Line-Attribute
+      color_end : AttValue -> Line-Attribute
+      linestyle : AttValue -> Line-Attribute
+      linewidth : AttValue -> Line-Attribute
+      fill : List(Fill-Attribute) * EMPTY -> Fill
+      tag : AttValue -> Fill-Attribute
+      class : AttValue -> Fill-Attribute
+      color : AttValue -> Fill-Attribute
+      colour : AttValue -> Fill-Attribute
+      fillstyle : AttValue -> Fill-Attribute
+      imagefill : AttValue -> Fill-Attribute
+      implementation : List(Implementation-Attribute) * EMPTY -> Implementation
+      tag : AttValue -> Implementation-Attribute
+      class : AttValue -> Implementation-Attribute
+      scriptname : AttValue -> Implementation-Attribute
--- aterm-tools/src/treeviz/treeviz.r
+++ aterm-tools/src/treeviz/treeviz.r
@@ -5,25 +5,40 @@ imports GraphXML lib
 strategies
 main = iowrap( treeviz )
 
-treeviz = 
-   collect( NodeToEdges );concat; ?edges;
-   !GraphXML( [], [graph([], edges )] )
-   
-
-NodeToEdges =
-   ?f#( args );
-   !args;
-   map(NodeToEdge(!target(<quote>f)))
-
-NodeToEdge(target) =
-   ?f#(_); 
-   !edge([source(<quote>f), <target>()] )
-
-quote =
-   explode-string
-   ; Quote
-   ; implode-string
 
 rules
-Quote:
-   chars       -> <concat>[[34],chars,[34]]
+treeviz:
+   f#(args) -> GraphXML( [], [graph([], edges )] )
+   where
+     <NewNodeName>f         => f';
+     <TreeViz>(f', f, args) => edges
+
+MkEdges( parent, x) :
+   f#(args) -> [edge|edges]
+   where
+     <NewNodeName>f                    => new-name;
+     <BuildEdge>(new-name, <parent>()) => edge;
+     <x>(new-name, f, args)            => edges
+
+NewNodeName :
+   f -> <concat-strings>[<new>(),  f]
+   
+BuildNode :
+   (nm, lbl) -> node([name(<double-quote>nm)],[label([], <double-quote>lbl)])
+
+BuildEdge :
+   (s, t) -> edge1([source(s), target(t)])
+
+strategies
+TreeViz =
+   rec x ({name, label, args, node, edges:
+      (
+         ?(name,label,[]);![]
+      <+
+         ?(name, label, args);
+         <map(MkEdges(!name,x));concat>args
+      )                        => edges;
+      <BuildNode>(name, label) => node; 
+      ![node|edges]
+   })
+
