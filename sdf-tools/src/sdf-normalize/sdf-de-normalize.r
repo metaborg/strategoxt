ed36f0807f1bf81f19dc044fbcd8d6a4a0a5ea32
Joost Visser <j.visser@sig.eu>
2001-08-16 17:46:56 +0000

    * sdf-de-normalize now also de-normalizes character classes      and literals.
    svn path=/trunk/StrategoXT/; revision=501

--- sdf-tools/src/sdf-normalize/sdf-de-normalize.r
+++ sdf-tools/src/sdf-normalize/sdf-de-normalize.r
@@ -44,11 +44,45 @@ strategies
       )
 
   de-norm-lex-symbol
-    = char-class(id)
-    + ?lit(_)
-    + \lex(symbol)->symbol\
+    = ( char-class(id)
+      + lit(id)
+      + \lex(symbol)->symbol\ )
+    ; alltd( lit(enquote) + de-norm-char-class )
 
   de-norm-cf-symbol
-    = char-class(id)
-    + ?lit(_)
-    + \cf(symbol)->symbol\
+    = ( char-class(id)
+      + lit(id)
+      + \cf(symbol)->symbol\ )
+    ; alltd( lit(enquote) + de-norm-char-class )
+
+  de-norm-char-class
+    = char-class(map(num2char <+ de-norm-range))
+    ; char-class(ranges2simple-cc)
+
+  de-norm-range
+    = range(num2char,num2char)
+
+  ranges2simple-cc
+    = foldr(!no-charranges,\(r,rs)->conc-ranges(r,rs)\)
+    ; \rs -> simple-charclass(rs)\
+
+  // Digits, upper- and lowercase letters are turned into ShortChars
+  // Newlines, tabs, and spaces are turned into ShortChars
+  // All others are turned into NumChars. 
+  num2char
+    = ( ?n; where(<geq>(n,48);<leq>(n,57)); <implode-string>[n] )
+   <+ ( ?n; where(<geq>(n,65);<leq>(n,90)); <implode-string>[n] )
+   <+ ( ?n; where(<geq>(n,97);<leq>(n,122)); <implode-string>[n] )
+   <+ \10 -> <implode-string>[92,110]\
+   <+ \9  -> <implode-string>[92,116]\
+   <+ \32 -> <implode-string>[92,32]\
+   <+ ( num-to-string; \str -> <concat-strings>["\\",str," "]\ )
+
+  num-to-string = 
+    rec x(split(sect(mod, !10); sect(add, !48), sect(div, !10); int);
+          (id, ?0; ![] <+ x); MkCons );
+    reverse;
+    implode-string
+
+  enquote
+    = \str -> <concat-strings>["\"",str,"\""]\
