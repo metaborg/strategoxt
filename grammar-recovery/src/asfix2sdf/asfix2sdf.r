f12a92c53f0af85430c755823a7505c26e78bfc7
Joost Visser <j.visser@sig.eu>
2001-08-16 20:48:04 +0000

    * Added --with-sdf-tools configuration switch.    * Added -d option to asfix2sdf to enable de-normalization of 	a reconstructed grammar.    * Added -g option to asfix2sdf to allow an initial grammar to 	be supplied. Instead of reconstructing a new grammar from the 	asfix input, this supplied grammar is minimized on the basis 	of the asfix input.
    svn path=/trunk/StrategoXT/; revision=503

--- grammar-recovery/src/asfix2sdf/asfix2sdf.r
+++ grammar-recovery/src/asfix2sdf/asfix2sdf.r
@@ -1,14 +1,137 @@
 module asfix2sdf
 imports
   lib Sdf-Syntax
+  sdf-de-normalize
 
 strategies
-  asfix2sdf
-    = iowrap(
-        collect-prods
-      ; prods2grammar
-      ; id2Id // Needed to keep pretty-printer happy
+
+  reconstruct-grammar
+    = collect-prods
+    ; prods2grammar
+    ; id2Id // Needed to keep pretty-printer happy
+
+  minimize-grammar
+    = ( ?tree
+      ; option-value(\GrammarName(str) -> str\,fail) => str
+      ; <file-exists>str  => file
+      ; <ReadFromFile>file => grammar
+      ; try-debug(<concat-strings>["Minimizing grammar ",str])
+      ; <collect-prods;de-normalize-productions>tree => prods
+      ; <topdown(try(mk-unused-prod-empty(!prods)))>grammar
+      ; eliminate-empty-prods
+      ; try-debug(!"Minimized grammar")
       )
+
+  maybe-de-normalize-grammar
+    = try( 
+        has-option(!DeNormalizeGrammar)
+      ; de-normalize-grammar 
+      ) 
+
+  de-normalize-productions
+    = id2Id // Needed to keep de-normalizer happy
+    ; remove-generated-prods;?prods
+    ; where (<filter(mk-prod-lex)>prods => lex-prods)
+    ; where (<filter(mk-prod-cf)>prods => cf-prods)
+    ; !(lex-prods,cf-prods) ; conc
+
+  de-normalize-grammar
+    = try-debug(!"De-normalizing grammar")
+    ; alltd(de-normalize-syntax)
+
+  de-normalize-grammar'
+    =  try-debug(!"De-normalizing grammar")
+    ; topdown( try( de-norm-char-class ) )
+    ; topdown( try( lit(enquote) ) )
+
+  mk-unused-prod-empty(prods)
+    = prod(id,id,id) => p 
+    ; prods          => ps
+    ; not(<getfirst(match-prod(!p))>ps)
+    ; !empty-prod()
+
+  match-prod(p)
+    = ?p2 
+    ; p => p1
+    ; ( <eq>(<remove-attrs>p1,<remove-attrs>p2) 
+     <+ <eq>(<get-constr>p1,<get-constr>p2)
+      )
+  
+  remove-attrs
+    = prod(id,id,!no-attrs)
+
+  get-constr
+    = \prod(_,_,atrs) -> c
+       where <oncetd(cons(?c))>atrs
+      \
+
+  eliminate-empty-prods
+    = innermost(
+       filter1(not(?empty-prod()))
+    <+ \simple-group(empty-prod())->prods-group([])\
+    )
+
+  filter1(s)
+    = ?xs;filter(s);not(?xs)
+
+
+
+strategies
+
+  asfix2sdf
+    = iowrap((id,asfix2sdf-main),asfix2sdf-options,asfix2sdf-usage)
+
+  asfix2sdf-main
+    = ( minimize-grammar )
+   <+ ( reconstruct-grammar
+      ; maybe-de-normalize-grammar 
+      )
+
+  asfix2sdf-options
+    = ArgOption("-g",\str -> GrammarName(str)\)
+    + Option("-d",!DeNormalizeGrammar)
+
+  asfix2sdf-usage
+    = where( option-defined(?Program(prog));
+             <printnl>
+             ( stderr,
+               [ prog, " -- reconstruct a minimal grammar from a parse tree\n",
+                 "\n",
+                 "usage : \n",
+                 "   ", prog, " [-S] [-i file] [-o file] [-b]\n",
+                 "   ", prog, " {--help|-h|-?}\n",
+                 "\n",
+                 "where\n",
+                 "   -d          de-normalize output grammar\n",
+                 "   -g grammar  minimize grammar\n",
+                 "   -S          run silently\n",
+                 "   -i file     read input from file (default: stdin)\n",
+                 "   -o file     write output to file (default: stdout)\n",
+                 "   -b          emit Binary ATerm (BAF) as output\n",
+                 "   -h          print help message\n",
+                 "\n",
+                 "   Input is a parse tree in AsFix format.\n",
+                 "\n",
+                 "   Output is an Sdf grammar (in AST form).\n",
+                 "\n",
+                 "   If an initial grammar (in AST form) is supplied with\n",
+                 "   the -g switch, then this grammar is minimized on the\n",
+                 "   basis of the input parse tree, i.e. layout, production\n",
+                 "   order, priorities, sort declarations, restrictions, etc.\n",
+                 "   are inherited from this grammar."
+               ] 
+             );
+             <exit> 1
+           )
+
+  try-debug(s) = try(not(has-option(!Silent));where(s;debug))
+
+signature
+  constructors
+    GrammarName		: String -> Option
+    DeNormalizeGrammar	:           Option
+
+strategies
 
   collect-prods
     = collect(is-prod)
