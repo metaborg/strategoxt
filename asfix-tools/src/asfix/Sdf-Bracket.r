ab7022333b628efe1565bacdb43c44bccee1a177
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2001-09-01 18:48:09 +0000

    * Added Sdf-Bracket.r module which contains constructor definition of   sdf-bracket
    svn path=/trunk/StrategoXT/; revision=602

---
+++ asfix-tools/src/asfix/Sdf-Bracket.r
@@ -0,0 +1,7 @@
+module Sdf-Bracket
+
+signature
+// sdf-bracket constructor. Used to indicate postions in AST's where during
+// pretty-printing brackets should be inserted
+constructors
+   sdf-bracket : S -> S
