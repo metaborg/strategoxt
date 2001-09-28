276904d86c78dadf5f752d853eb041ab9ce8ed55
Eelco Dolstra <eelco.dolstra@logicblox.com>
2001-09-28 22:14:47 +0000

    Recognize groups of productions that can be expressed by means of a regular expression. Assumptions: (1) all productions for a sort are in one context-free syntax section (2) all productions are available. Sorts representing regular expressions are replaced by these expressions to improve the abstract syntax of the language (no superfluous injections).
    svn path=/trunk/StrategoXT/; revision=679

---
+++ sdf-tools/src/sdf2sdf/sdf-regularize.r
@@ -0,0 +1,135 @@
+\literate[sdf-regularize]
+
+	\begin{absract}
+	Recognize groups of productions that can be expressed by means of
+	a regular expression. Assumptions: (1) all productions for a sort are
+	in one context-free syntax section (2) all productions are available.
+	Sorts representing regular expressions are replaced by these expressions
+	to improve the abstract syntax of the language (no superfluous injections).
+	\end{abstract}
+
+\begin{code}
+module sdf-regularize
+imports lib Sdf-Syntax dynamic-rules
+
+strategies
+
+  sdf-regularize =
+    iowrap(regularize)
+
+  regularize = 
+    alltd(context-free-syntax(order-productions));          
+    alltd(RegularizeList <+ RegularizeOptional);
+    inline-lists-and-optionals
+
+strategies
+
+  order-productions = 
+    sort-list(SortL(ProdGT))
+
+  ProdGT =
+    ?(prod(as, a, _), prod(as', a, _));
+    where(<gt>(<length> as, <length>as'))
+
+  ProdGT =
+    ?(prod(as, a, _), prod(bs, b, _));
+    where(<not(eq); string-gt>(a, b))
+
+rules
+
+  // Lists
+
+  RegularizeList : // a* -> b
+     [prod([],   b,no-attrs),
+      prod([b,a],b,no-attrs)]
+  -> [prod([iter-star(a)],b,no-attrs)]
+
+  RegularizeList : // a* -> b
+    [prod([],   b,no-attrs),
+     prod([a,b],b,no-attrs)]
+  ->
+    [prod([iter-star(a)],b,no-attrs)]
+
+  RegularizeList : // a* -> b
+    [prod([],   b,no-attrs),
+     prod([a],  b,no-attrs),
+     prod([b,a],b,no-attrs)]
+  ->
+    [prod([iter-star(a)],b,no-attrs)]
+
+  RegularizeList : // a+ -> b
+    [prod([a],  b,no-attrs),
+     prod([b,a],b,no-attrs)]
+  ->
+    [prod([iter(a)],b,no-attrs)]
+
+  RegularizeList : // (a|c)* -> b
+    [prod([],   b,no-attrs),
+     prod([b,a],b,no-attrs),
+     prod([b,c],b,no-attrs)]
+  ->
+    [prod([iter-star(alt(a,c))],b,no-attrs)]
+
+  // Lists of seqences
+
+  RegularizeList : // (a as)* -> b
+    [prod([],          b,no-attrs),
+     prod([b, a | as], b,no-attrs)]
+  -> 
+    [prod([iter-star(seq(a, as))],b,no-attrs)]
+    where <not([])> as
+
+  // Lists with separators
+
+  RegularizeList : // {A B}+
+    [prod([],       b,no-attrs),
+     prod([a],      b,no-attrs),
+     prod([a,sep,b],b,no-attrs)]
+  ->
+    [prod([iter-star-sep(a, sep)],b,no-attrs)]
+
+  RegularizeList : // {A B}*
+    [prod([a],      b,no-attrs),
+     prod([a,sep,b],b,no-attrs)]
+  ->
+    [prod([iter-sep(a, sep)],b,no-attrs)]
+
+  RegularizeList : // {A B}*
+    [prod([a],      b,no-attrs),
+     prod([b,sep,a],b,no-attrs)]
+  ->
+    [prod([iter-sep(a, sep)],b,no-attrs)]
+
+  // Optionals
+
+  RegularizeOptional : // a?
+    [prod([], b,no-attrs),
+     prod([a],b,no-attrs)]
+  -> 
+    [prod([opt(a)],b,no-attrs)]
+
+  RegularizeOptional : // (a1 a2 ..)?
+    [prod([],           b,no-attrs),
+     prod([a1, a2 | as],b,no-attrs)]
+  -> 
+    [prod([opt(seq(a1, [a2 | as]))],b,no-attrs)]
+
+strategies
+
+  inline-lists-and-optionals =
+    alltd(find-inlinebles);
+    alltd(prod(alltd(ReplaceSymbol),id,id))
+
+  find-inlinebles : 
+    context-free-syntax([prod([a],b,no-attrs)]) -> context-free-syntax([])
+    where <inlineable> a
+        ; rules(ReplaceSymbol : b -> a)
+
+  find-inlinebles : 
+    context-free-syntax([prod([opt(seq(a,as))],b,no-attrs)]) -> 
+    context-free-syntax([prod([a | as],b,no-attrs)])
+    where rules(ReplaceSymbol : b -> opt(b))
+
+  inlineable = 
+    opt(not(seq(id,id))) + iter(id) + iter-star(id) + iter-sep(id,id) + iter-star-sep(id,id)
+\end{code}
