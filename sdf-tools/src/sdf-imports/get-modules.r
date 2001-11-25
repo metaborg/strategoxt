dc47861f5cc08fc9157ad56ad22c123fd6d29a17
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2001-11-25 22:12:48 +0000

    * sdf-imports/get-modules.r: fixed incorrect grammar identifier
    svn path=/trunk/StrategoXT/; revision=863

--- sdf-tools/src/sdf-imports/get-modules.r
+++ sdf-tools/src/sdf-imports/get-modules.r
@@ -78,7 +78,7 @@ strategies
              collect( \ Edge(x, y) -> edge1([source(<quote>y), 
                                       target(<quote>x)]) \ ) ); 
       ?(nodes, edges); 
-      <mkterm>("\"graphxml_1_1.0\"", [GraphXML( [], [graph([], <conc>(nodes, edges))])])
+      <mkterm>("\"graphxml_1_1-0\"", [GraphXML( [], [graph([], <conc>(nodes, edges))])])
    <+
       filter( \Node(x) -> x\ ) 
    );
