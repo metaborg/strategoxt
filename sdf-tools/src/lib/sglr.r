80a0477b9ec7250b9eb1125cbb48dabe40c097db
Eelco Dolstra <eelco.dolstra@logicblox.com>
2002-03-16 23:09:16 +0000

    refactoring
    svn path=/trunk/StrategoXT/; revision=1096

--- sdf-tools/src/lib/sglr.r
+++ sdf-tools/src/lib/sglr.r
@@ -23,14 +23,16 @@ imports exec gt-paths asfix
 
 rules
 
-  sglr : (tbl, 'in, out) -> out
-	 where
-           <call> (<concat-strings>[SGLR, "/bin/sglr"], ["-2", "-p", tbl, "-i", 'in, "-o", out])
+  sglr : 
+    (tbl, 'in, out) -> out
+    where
+      <call> (<concat-strings>[SGLR, "/bin/sglr"], ["-2", "-p", tbl, "-i", 'in, "-o", out])
 
 
-  sglr : (tbl, 'in) -> tree
- 	 where
-	  <conc-strings> ("/tmp/sglr", <get-pid; int-to-string>()) => out; 
-	  <sglr> (tbl, 'in, out);
-	  <ReadFromFile> out => parsetree(tree,_);
-	  <rm-files> [out]
+  sglr : 
+    (tbl, 'in) -> tree
+    where
+      <conc-strings> ("/tmp/sglr", <get-pid; int-to-string>()) => out; 
+      <sglr> (tbl, 'in, out);
+      <ReadFromFile> out => parsetree(tree,_);
+      <rm-files> [out]
