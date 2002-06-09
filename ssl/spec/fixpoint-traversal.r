19f977a6639a39fc6d684922d5bf16f8b729fdfa
Stratego <developers@strategoxt.org>
2002-06-09 09:02:07 +0000

    innermost-tagged uses annotations to tag terms that are in normal form.
    svn path=/trunk/StrategoXT/; revision=1216

--- ssl/spec/fixpoint-traversal.r
+++ ssl/spec/fixpoint-traversal.r
@@ -8,23 +8,6 @@
 
 	\end{abstract}
 
-% Copyright (C) 1998-2001 Eelco Visser <visser@acm.org>
-% 
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2, or (at your option)
-% any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-% 02111-1307, USA.
-
 \begin{code}
 module fixpoint-traversal
 imports iteration simple-traversal memo
@@ -42,6 +25,9 @@ strategies
     repeat(oncebu(s))
 
   innermost(s)  = 
+    bottomup(rec x(try(s; bottomup(x))))
+
+  innermost-old(s) =
     rec x(all(x); (s; x <+ id))
 
   pseudo-innermost3(s) =
@@ -49,5 +35,30 @@ strategies
 
   innermost-memo(s) =
     rec x(memo(all(x); (s; x <+ id)))
+
+  // innermost-tagged(s) reduces the subject term by applying s to
+  // innermost redices first. Terms in normal form are tagged (using
+  // attributes) to prevent renormalization.
+
+  innermost-tagged(s : a -> a) = // : a -> a
+    where(new => tag);
+    rec x(?_{tag} <+ (all(x); (s; x <+ !<id>{tag})));
+    bottomup(?<id>{tag})
 \end{code}
 
+% Copyright (C) 1998-2002 Eelco Visser <visser@acm.org>
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
