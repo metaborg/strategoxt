235dd6bff520f860b70f25c013151c4c71efe79b
Stratego <developers@strategoxt.org>
2002-06-09 08:58:14 +0000

    support for time measurements extended with primitives calling the 	system calls clock and time. Module time defines a number of 	useful abstractions over this. In particular, <run-time> returns 	the seconds of total run-time so far, including the run-time of 	any child processes.  The operator profile(msg, s) measures the 	time used to apply s and then prints user and system time, 	including children time.
    svn path=/trunk/StrategoXT/; revision=1196

--- ssl/spec/time.r
+++ ssl/spec/time.r
@@ -6,7 +6,72 @@
 	strategies.
 	\end{abstract}
 
-% Copyright (C) 1998-2001 Eelco Visser <visser@acm.org>
+\begin{code}
+module time
+strategies
+
+  dtime = 
+    prim("SSL_dtime")
+
+  clock-to-seconds = 
+    ?c; prim("SSL_ClockToSeconds", c)
+
+  clock = 
+    prim("SSL_clock")
+
+  times = 
+    prim("SSL_times")
+
+  ticks-to-seconds = 
+    ?t; prim("SSL_TicksToSeconds", t)
+
+  diff-times :
+    ((a1, b1, c1, d1), (a2, b2, c2, d2)) -> 
+    (<subt>(a1, a2), <subt>(b1, b2), <subt>(c1, c2), <subt>(d1, d2))
+
+  add-times :
+    ((a1, b1, c1, d1), (a2, b2, c2, d2)) -> 
+    (<add>(a1, a2), <add>(b1, b2), <add>(c1, c2), <add>(d1, d2))
+
+  times-to-seconds :
+    (a, b, c, d) -> 
+    (<ticks-to-seconds> a, <ticks-to-seconds> b, <ticks-to-seconds> c, <ticks-to-seconds> d)
+
+  user-time :
+    (a, b, c, d) -> a
+
+  system-time :
+    (a, b, c, d) -> b
+
+  cuser-time :
+    (a, b, c, d) -> c
+
+  csystem-time :
+    (a, b, c, d) -> d
+
+  self-children-user-time :
+    (a, b, c, d) -> <add>(a, c)
+
+  self-children-sys-time :
+    (a, b, c, d) -> <add>(b, d)
+
+  // total run-time so far
+  run-time =
+    times; crush(!0, add); ticks-to-seconds
+
+  profile(msg, s) =
+    where(times => start)
+    ; s
+    ; where(
+        <diff-times
+         ; <print> (stderr, [<msg>
+                             , " user ", <self-children-user-time; ticks-to-seconds>
+                             , " system ", <self-children-sys-time; ticks-to-seconds>
+			     , "\n"])> (<times>, start)
+      )
+\end{code}
+
+% Copyright (C) 1998-2002 Eelco Visser <visser@acm.org>
 % 
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -22,10 +87,3 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 % 02111-1307, USA.
-
-\begin{code}
-module time
-strategies
-
-  dtime = prim("SSL_dtime")
-\end{code}
