4f1358a849e4eeea26802e57afd22ceb9d300caa
Stratego <developers@strategoxt.org>
2002-06-09 10:33:21 +0000

    copyright notice
    svn path=/trunk/StrategoXT/; revision=1245

--- ssl/spec/getopt.r
+++ ssl/spec/getopt.r
@@ -1,19 +1,4 @@
 \literate{getopt}
-%    Copyright (C) 2002  Otto Skrove Bagge
-%
-%    This program is free software; you can redistribute it and/or modify
-%    it under the terms of the GNU General Public License as published by
-%    the Free Software Foundation; either version 2 of the License, or
-%    (at your option) any later version.
-%
-%    This program is distributed in the hope that it will be useful,
-%    but WITHOUT ANY WARRANTY; without even the implied warranty of
-%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%    GNU General Public License for more details.
-%
-%    You should have received a copy of the GNU General Public License
-%    along with this program; if not, write to the Free Software
-%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
 	This module parses command line arguments and build an option table
@@ -183,7 +168,9 @@ signature
     OptArgName: String -> Option
 
     OptRest:  String -> Option
+
 strategies
+
   getopt-demo =
 	getopt-parse(getopt-my-options,
 		!["getopt [options] file ...", "getopt -h"],
@@ -479,3 +466,21 @@ strategies
   getopt-display = 
 	?"version";
 	<getopt; error> "getopt-version-string"
+
+\end{code}
+
+%    Copyright (C) 2002  Otto Skrove Bagge
+%
+%    This program is free software; you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation; either version 2 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program; if not, write to the Free Software
+%    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
