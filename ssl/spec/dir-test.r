dbc649ec1745b7d34b64f016f35c6d8c497473af
Stratego <developers@strategoxt.org>
2002-06-09 09:16:26 +0000

    <readdir> d gives list of filenames of directory d
    svn path=/trunk/StrategoXT/; revision=1232

---
+++ ssl/spec/dir-test.r
@@ -0,0 +1,40 @@
+\literate[dir-test]
+
+	\begin{abstract}
+	A dir module for definition of unit tests.
+	\end{abstract}
+
+\begin{code}
+module dir-test
+imports sunit dir
+strategies
+
+  main = 
+    test-suite(!"dir-test", 
+      test1
+    )
+
+  test1 =
+    apply-test(!"test1"
+	, readdir; fetch-elem(?"dir-test.c")
+	, !"."
+	, !"dir-test.c"
+	)
+\end{code}
+
+% Copyright (C) 2002 Eelco Visser <visser@acm.org>
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
