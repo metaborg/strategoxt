b9dd331ccbe47095fa469481c5b09cd2ec4ea050
Stratego <developers@strategoxt.org>
2002-06-09 15:32:28 +0000

    signature for list variables
    svn path=/trunk/StrategoXT/; revision=1267

--- sc/spec/sig/sugar.r
+++ sc/spec/sig/sugar.r
@@ -1,22 +1,5 @@
 \literate[Syntactic Sugar]
 
-% Copyright (C) 1998, 1999, 2000 Eelco Visser <visser@acm.org>
-% 
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 2, or (at your option)
-% any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-% 02111-1307, USA.
-
 
 	In this section we define specifications that declare
 	signatures, rules and strategy definitions. This is just
@@ -71,6 +54,8 @@ signature
     BuildDefault : Term                -> Term    (* _ t *)
 
     Anno         : Term * Term         -> Term    (* t { t } *)
+ 
+    ListVar	 : String	       -> String  (* t* *)
 \end{code}
 
 	\paragraph{Rules \& Rule Definitions}
@@ -121,3 +106,20 @@ signature
     Imports       : List(String)  -> BSpec
     Specification : List(BSpec)   -> Spec
 \end{code}
+
+% Copyright (C) 1998-2002 Eelco Visser <visser@acm.org>
+% 
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2, or (at your option)
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+% 02111-1307, USA.
