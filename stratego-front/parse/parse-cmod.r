2a214de7aa82f841eb4947b99c393e07469afe78
Stratego <developers@strategoxt.org>
2002-06-10 21:18:25 +0000

    verbosity levels
    svn path=/trunk/StrategoXT/; revision=1328

--- stratego-front/parse/parse-cmod.r
+++ stratego-front/parse/parse-cmod.r
@@ -1,5 +1,5 @@
 module parse-cmod
-imports lib config
+imports lib config verbose
 
 signature
   constructors
@@ -9,16 +9,18 @@ strategies
 
   parse-cmod =
     where([get-path => prefix | id])
-    ; where(prim("get_conf_pkgdatadir") => datadir)
+    ; where(prim("get_conf_pkgdatadir") => datadir) 
     ; import-config-file(
         find-config-file(![prefix,datadir], !"parse-cmod.config")
       )
     ; parse-options(io-options 
         + ArgOption("-I" + "--Include", where(<extend-config>("-I", [<id>])),
-	            !"-I p|--Include p include modules from directory p")) => options
+	            !"-I p|--Include p include modules from directory p") 
+        + Option("-silent", where(<set-config>("--verbose", 0)),
+	            !"-silent ")) => options
     ; <extend-config> ("-I", ["./"])
     ; <get-config> "-i" => infile
-    ; debug(!"parsing ")
+    ; if-verbose2(debug(!"parsing "))
     ; (<get-config> "-o" <+ new-file) => outfile
     ; <parse-concrete-module> (infile, outfile)
     ; (<get-config> "-o" 
@@ -33,10 +35,10 @@ strategies
     ; <get-parse-table> syntax => tbl
     ; <basename; guarantee-extension(!"atree")> filein => fileout2
     ; !filein
-    ; call-tmp(<get-config> "sglr",             !["-2", "-p", tbl],
-      call-out(<get-config> "implode-asfix",    ![], !fileout2,
-      call-tmp(<get-config> "meta-explode",     ![],
-      call-out(<get-config> "stratego-desugar", ![], !fileout, id))))
+    ; call-tmp(<get-config> "sglr",             !["-2", "-p", tbl | <![]; if-verbose3(!["-v"])> ],
+      call-out(<get-config> "implode-asfix",    ![]; if-less-verbose1(!["-S"]), !fileout2,
+      call-tmp(<get-config> "meta-explode",     !["--verbose", <verbosity; int-to-string>],
+      call-out(<get-config> "stratego-desugar", !["--verbose", <verbosity; int-to-string>], !fileout, id))))
 
   get-syntax = 
      basename
@@ -48,8 +50,8 @@ strategies
   get-parse-table =
     ? syntax
     ; <guarantee-extension(!"tbl")> syntax => tbl1
-    ; ( <find-in-path; debug(!"using parse table: ")> (tbl1, <get-config> "-I")
-       <+ say(!"creating parse table")
+    ; ( <find-in-path; if-verbose2(debug(!"using parse table: "))> (tbl1, <get-config> "-I")
+       <+ if-verbose2(say(!"creating parse table"))
           ; <get-syntax-definition> syntax => def
           ; <call> (<get-config>"sdf2table", ["-i", def, "-o", tbl1, "-m", syntax])
           ; !tbl1
@@ -58,8 +60,8 @@ strategies
   get-syntax-definition =
     ? syntax
     ; <guarantee-extension(!"def")> syntax => def
-    ; ( <find-in-path; debug(!"using syntax definition: ")> (def, <get-config> "-I")
-        <+ say(!"creating syntax definition")
+    ; ( <find-in-path; if-verbose2(debug(!"using syntax definition: "))> (def, <get-config> "-I")
+        <+ if-verbose2(say(!"creating syntax definition"))
            ; <guarantee-extension(!"sdf")> syntax => sdf
 	   ; temp-file( \ (adef, _) -> <
                 <call> (<get-config>"pack-sdf", 
