b07036bfa8e4ebdcf24050e3b4701ba37eb4ad54
Stratego <developers@strategoxt.org>
2002-12-08 22:14:06 +0000

    using autoxt
    svn path=/trunk/StrategoXT/; revision=1634

--- obsolete/cgen/sig/C.r
+++
@@ -1,5 +0,0 @@
-module C
-imports Keywords Constants Lexical Preprocessor Declarators Program Statements
-        Identifiers Types Expressions StructsAndUnions Enumerators
-        ParameterDeclarators AbstractDeclarators
-	options
