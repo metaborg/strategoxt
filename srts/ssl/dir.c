389e0c402382b556f33e4c2ad7015cacd8da8930
Rob Vermaas <rob.vermaas@gmail.com>
2003-06-25 09:07:33 +0000

    removed MAP_FILE from dir.c
    svn path=/trunk/StrategoXT/; revision=3062

--- srts/ssl/dir.c
+++ srts/ssl/dir.c
@@ -193,7 +193,7 @@ ATerm SSL_copy_mmap(ATerm oldname, ATerm newname)
       _fail(oldname);
     }
 
-  if( (src = mmap(0, statbuf.st_size, PROT_READ, MAP_FILE | MAP_SHARED, fdin, 0))
+  if( (src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, fdin, 0))
       == (caddr_t) - 1)
     {
       ATfprintf(stderr, "SSL_copy: mmap error for input\n", newname);
@@ -201,7 +201,7 @@ ATerm SSL_copy_mmap(ATerm oldname, ATerm newname)
     }
 
   if( (dst = mmap(0, statbuf.st_size, PROT_READ | PROT_WRITE, 
-		  MAP_FILE | MAP_SHARED, fdout, 0)) == (caddr_t) - 1)
+		  MAP_SHARED, fdout, 0)) == (caddr_t) - 1)
     {
       ATfprintf(stderr, "SSL_copy: mmap error for output\n", newname);
       _fail(oldname);
