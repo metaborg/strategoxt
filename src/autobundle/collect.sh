3f7266ccc15045afb04883efaa50d353fdca40b1
Merijn de Jonge <merijn.de.jonge@sorama.eu>
2004-04-20 15:55:45 +0000

    * autobundle/collect.sh: Added support for subversion. Refactored script to   prevent code duplication
    svn path=/branches/autobundle-timed-dependencies/; revision=5867

--- src/autobundle/collect.sh
+++ src/autobundle/collect.sh
@@ -2,7 +2,7 @@
 #
 # Autobundle -- Tool suite for automated source tree composition
 #
-# Copyright (C) 2002-2003 Merijn de Jonge <M.de.Jonge@tue.nl>
+# Copyright (C) 2002-2004 Merijn de Jonge <mdejonge@cs.uu.nl>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -37,6 +37,7 @@
 #   cvs://cvs..:/CVS    Specifies that the package should be build by first
 #                       checking it out from the repository and then
 #                       executing a "make dist" to build a distribution
+#   svn://https://..../ Checkout package from SubVersion repository
 #   http://www.cw...    Obtain a complete distribution via http
 #   ftp://ftp.cw...     Obtain a complete distribution via ftp
 #   file:///home/x/...  Obtain a distribution from _local_ file system
@@ -80,16 +81,84 @@ PKG_LIST=pkg-list       # Default  package list file
 FORCE=false             # Force package retrieval when package is already available
 PKGS=                   # Empty package list
 
-# Don't throw away any cvs checked out packages, but save them
-# in the directoy ${_pkg}-cvs
-save_cvs () {
+# Don't throw away any packages that are checked out from a version control
+# system, but save them in a specific  directoy (e.g., ${_pkg}-cvs)
+save_vcs () {
    _pkg=$1
    if [ -d ./${_pkg}/CVS ]; then
       echo "A copy of the CVS checked out version of"\
            "${_pkg} is saved in ${_pkg}-cvs." >&2
       mv ${_pkg} ${_pkg}-cvs
+   elif [ -d ./${_pkg}/.svn ]; then
+      echo "A copy of the SubVersion checked out version of"\
+           "${_pkg} is saved in ${_pkg}-svn." >&2
+      mv ${_pkg} ${_pkg}-svn
    fi
+}
 
+# Restore a previously saved checked-out version
+restore_vcs () {
+   pkg=$1
+   pkg_version=$2
+   
+   # If we already have the package checked out using SubVersion, perform a
+   # svn update and remove the version that we just checked out
+   if [ -d ${pkg}-${pkg_version} -a -d ${pkg}-${pkg_version}/.svn ]; then
+   (
+      rm -fr ./${pkg}
+      cd ${pkg}-${pkg_version}
+      svn update
+   )
+   # Or, if we already have the package checked out using CVS, perform a cvs
+   # update and remove the version that we just checked out
+   elif [ -d ${pkg}-${pkg_version} -a -d ${pkg}-${pkg_version}/CVS ]; then
+   (
+      rm -fr ./${pkg}
+      cd ${pkg}-${pkg_version}
+      cvs update
+   )
+   # otherwise, rename the checked out version to pkg-version
+   else
+      rm -fr ${pkg}-${pkg_version}
+      mv ${pkg} ${pkg}-${pkg_version}
+   fi
+}
+
+# Set-up the build environment for a package that has been checked-out from 
+# a version control system
+vcs_init() {
+   pkg=$1
+   
+   # initialized autotools build system
+   init_autotools ${pkg}
+
+   # retrieve package version
+   autotools_pkg_version ${pkg}
+  
+   # restore (if possible) a previously checked-out version
+   restore_vcs ${pkg} ${pkg_version}
+}
+
+# create configure script and Makefile.in's 
+init_autotools()
+{
+   pkg=$1
+   (  cd ${pkg}; aclocal; autoheader; libtoolize; automake -a; autoconf ) \
+      2>/dev/null 1>/dev/null
+}
+
+# Determine package version from configure.ac or configure.in in
+# case it was defined in the pkg-list file as '*'.
+autotools_pkg_version()
+{
+   pkg=$1
+   if [ -f ${pkg}/configure.ac ]; then
+      CONFIG_FILE=${pkg}/configure.ac
+   else
+      CONFIG_FILE=${pkg}/configure.in
+   fi
+   pkg_version=`grep AC_INIT ${CONFIG_FILE} \
+                   | cut -d, -f2 | tr -d '[ ]' | sed 's/)$//'`
 }
 
 do_collect () {
@@ -99,6 +168,16 @@ do_collect () {
 
 
    case "${pkg_url}" in
+      svn://* )
+         echo "Obtaining \"${pkg}\" from SubVersion.">&2
+         svnroot=`echo ${pkg_url} | sed 's@svn://@@g'`
+
+         svn checkout -q ${svnroot}/${pkg}
+        
+         vcs_init ${pkg}
+
+         ;;
+        
       cvs://* )
          echo "Obtaining \"${pkg}\" from CVS.">&2
          cvsroot=`echo ${pkg_url} | sed 's@cvs://@@g'`
@@ -106,48 +185,13 @@ do_collect () {
          # Obtain sources using cvs checkout
          cvs -Q -d ${cvsroot} checkout ${pkg}
 
-         # create configure script and Makefile.in's 
-         (  cd ${pkg}; aclocal; autoheader; libtoolize; automake -a; autoconf ) \
-            2>/dev/null 1>/dev/null
-
-         # Determine package version from configure.ac or configure.in in
-         # case it was defined in the pkg-list file as '*'.
-         if [ "a${pkg_version}" = "a*" ]; then
-            if [ -f ${pkg}/configure.ac ]; then
-               CONFIG_FILE=${pkg}/configure.ac
-            else
-               CONFIG_FILE=${pkg}/configure.in
-            fi
-            pkg_version=`grep AC_INIT ${CONFNIG_FILE} \
-                            | cut -d, -f2 | tr -d '[ ]' | sed 's/)$//'`
-         fi
+         vcs_init ${pkg}
          
-         # If we have already a saved CVS copy; rename it to
-         # ${pkg}-${pkg_version} such that a cvs update command can be
-         # performed.
-         if [ -d ${pkg}-${pkg_version}-cvs -a -d ${pkg}-${pkg_version}-cvs/CVS ]; then
-            rm -fr ./${pkg}-${pkg_version}
-            mv ${pkg}-${pkg_version}-cvs ${pkg}-${pkg_version}
-         fi
-            
-         # If we already have the package checked out, perform a cvs upd and
-         # remove the version that we just checked out
-         if [ -d ${pkg}-${pkg_version} -a -d ${pkg}-${pkg_version}/CVS ]; then
-         (
-            rm -fr ./${pkg}
-            cd ${pkg}-${pkg_version}
-            cvs upd -d
-         )
-         # otherwise, rename the checked out version to pkg-version
-         else
-            rm -fr ${pkg}-${pkg_version}
-            mv ${pkg} ${pkg}-${pkg_version}
-         fi
          ;;
 
       file://* | http://* | ftp://* )
          # Save cvs checked out versions
-         save_cvs ${pkg}-${pkg_version}
+         save_vcs ${pkg}-${pkg_version}
          
          protocol="`echo ${pkg_url} | cut -d: -f 1 | tr [:lower:] [:upper:]`"
 
