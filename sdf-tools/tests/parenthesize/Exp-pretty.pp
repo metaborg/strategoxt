65c4715aef8f04f99291634d267c08b0c7ee8af8
Martin Bravenboer <martin.bravenboer@gmail.com>
2004-05-27 08:13:31 +0000

    Testing infrastructure for sdf2parenthesize
    svn path=/trunk/StrategoXT/; revision=6339

---
+++ sdf-tools/tests/parenthesize/Exp-pretty.pp
@@ -0,0 +1,8 @@
+[
+   Mul  -- H hs=1 [_1 "*" _2],
+   Div  -- H hs=1 [_1 "/" _2],
+   Plus -- H hs=1 [_1 "+" _2],
+   Min  -- H hs=1 [_1 "-" _2],
+   Var  -- _1,
+   Parenthetical -- H hs=0 ["(" _1 ")"]
+]
