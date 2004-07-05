71ce22e915f30d7f4f8ed5575d13b170b186f86a
Martin Bravenboer <martin.bravenboer@gmail.com>
2004-07-05 13:40:14 +0000

    Bootstrap and separate compilation
    svn path=/trunk/StrategoXT/; revision=6687

--- gpp/xtc-tools/gen-css-boxstyle.c
+++ gpp/xtc-tools/gen-css-boxstyle.c
@@ -14,9 +14,9 @@ static void init_module_constant_terms (void)
 }
 #include <srts/init-stratego-application.h>
 ATerm xtc_find_file_0_0 (ATerm t);
-ATerm xtc_output_1_0 (ATerm v_106 (ATerm), ATerm t);
+ATerm xtc_output_1_0 (ATerm w_106 (ATerm), ATerm t);
 ATerm output_options_0_0 (ATerm t);
-ATerm option_wrap_2_0 (ATerm s_108 (ATerm), ATerm t_108 (ATerm), ATerm t);
+ATerm option_wrap_2_0 (ATerm u_108 (ATerm), ATerm v_108 (ATerm), ATerm t);
 static ATerm a_0 (ATerm t);
 static ATerm b_0 (ATerm t);
 ATerm main_0_0 (ATerm t);
