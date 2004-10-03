6a0af8e51011539657e16d57d1d6b5d6e093d727
Martin Bravenboer <martin.bravenboer@gmail.com>
2004-10-03 08:15:59 +0000

    More samples
    svn path=/branches/StrategoXT-0.12/; revision=7546

--- xml-front/doc/demo.sh
+++ xml-front/doc/demo.sh
@@ -2,23 +2,25 @@ function demoterm() {
   echo "<tr>" >> $out
 
   echo "<td>" >> $out
-  echo "$term" >> $out
+  echo "<verbatim>" >> $out
+  echo "`echo $term | pp-aterm`" >> $out
+  echo "</verbatim>" >> $out
   echo "</td>" >> $out
 
   echo "<td>" >> $out
-  xml=`echo "$term" | aterm2xml`
+  xml=`echo "$term" | aterm2xml $mode`
   xmlwf=`echo "$xml" | xmllint --format -`
   if test $? -ne 0; then
     echo "not possible" >> $out
   else
     echo "<verbatim>" >> $out
-    echo "$xmlwf" >> $out
+    echo "`echo "$xmlwf" | tail +2`" >> $out
     echo "</verbatim>" >> $out
   fi
   echo "</td>" >> $out
 
   echo "<td>" >> $out
-  roundterm=`echo "$xmlwf" | xml2aterm`
+  roundterm=`echo "$xmlwf" | xml2aterm $mode`
   if test $? -ne 0; then
     echo "not possible" >> $out
   else
@@ -31,6 +33,9 @@ function demoterm() {
 }
 
 function demoterms() {
+  rm $out
+  touch $out
+
   term="foo"     demoterm
   term="foo(1)"  demoterm
   term="1"       demoterm
@@ -40,11 +45,12 @@ function demoterms() {
   term="[]"      demoterm
   term="[1, 2]"  demoterm
   term="fred([foo(), bar()])" demoterm
+  term="fred(None(), [foo(), bar()])" demoterm
+  term="fred(Some(barney()), [foo(), bar()])" demoterm
   term="fred(\"foo\", \"bar\")" demoterm
+  term="foo(){fred}" demoterm
 }
 
-out=implicit.html
-rm $out
-touch implicit.html
-
-demoterms
+out=implicit.html      mode="--implicit"       demoterms
+out=very-explicit.html mode="--very-explicit"  demoterms
+out=explicit.html      mode="--explicit"       demoterms
