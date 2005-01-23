f1d570d80205d64b2f48c0c4b668e5047388cf3e
Martin Bravenboer <martin.bravenboer@gmail.com>
2005-01-23 12:04:06 +0000

    Un-double-quote string literals after parsing
    svn path=/trunk/StrategoXT/; revision=8710

--- gpp/xtc-tools/parse-box.c
+++ gpp/xtc-tools/parse-box.c
@@ -45,6 +45,9 @@ static Symbol sym__15;
 static Symbol sym__16;
 static Symbol sym__17;
 static Symbol sym__18;
+static Symbol sym_Cons_2;
+static Symbol sym_Nil_0;
+static Symbol sym_S_1;
 static void init_module_constructors (void)
 {
   sym_Nil_0 = ATmakeSymbol("Nil", 0, ATfalse);
@@ -131,44 +134,57 @@ static void init_module_constructors (void)
   ATprotectSymbol(sym__17);
   sym__18 = ATmakeSymbol("", 18, ATfalse);
   ATprotectSymbol(sym__18);
+  sym_Cons_2 = ATmakeSymbol("Cons", 2, ATfalse);
+  ATprotectSymbol(sym_Cons_2);
+  sym_Nil_0 = ATmakeSymbol("Nil", 0, ATfalse);
+  ATprotectSymbol(sym_Nil_0);
+  sym_S_1 = ATmakeSymbol("S", 1, ATfalse);
+  ATprotectSymbol(sym_S_1);
 }
+static ATerm term_w_0;
+static ATerm term_v_0;
+static ATerm term_u_0;
+static ATerm term_t_0;
+static ATerm term_s_0;
+static ATerm term_r_0;
 static ATerm term_q_0;
-static ATerm term_p_0;
-static ATerm term_o_0;
-static ATerm term_n_0;
-static ATerm term_m_0;
-static ATerm term_l_0;
-static ATerm term_j_0;
 static void init_module_constant_terms (void)
 {
-  ATprotect(&(term_j_0));
-  term_j_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-v", 0, ATtrue));
-  ATprotect(&(term_l_0));
-  term_l_0 = (ATerm) ATmakeAppl(ATmakeSymbol("sglr", 0, ATtrue));
-  ATprotect(&(term_m_0));
-  term_m_0 = (ATerm) ATmakeAppl(sym__0);
-  ATprotect(&(term_n_0));
-  term_n_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-2A", 0, ATtrue));
-  ATprotect(&(term_o_0));
-  term_o_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-p", 0, ATtrue));
-  ATprotect(&(term_p_0));
-  term_p_0 = (ATerm) ATmakeAppl(ATmakeSymbol("Box.tbl", 0, ATtrue));
   ATprotect(&(term_q_0));
-  term_q_0 = (ATerm) ATmakeAppl(ATmakeSymbol("implode-asfix", 0, ATtrue));
+  term_q_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-v", 0, ATtrue));
+  ATprotect(&(term_r_0));
+  term_r_0 = (ATerm) ATmakeAppl(ATmakeSymbol("sglr", 0, ATtrue));
+  ATprotect(&(term_s_0));
+  term_s_0 = (ATerm) ATmakeAppl(sym__0);
+  ATprotect(&(term_t_0));
+  term_t_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-2A", 0, ATtrue));
+  ATprotect(&(term_u_0));
+  term_u_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-p", 0, ATtrue));
+  ATprotect(&(term_v_0));
+  term_v_0 = (ATerm) ATmakeAppl(ATmakeSymbol("Box.tbl", 0, ATtrue));
+  ATprotect(&(term_w_0));
+  term_w_0 = (ATerm) ATmakeAppl(ATmakeSymbol("implode-asfix", 0, ATtrue));
 }
 #include <srts/init-stratego-application.h>
+ATerm un_double_quote_0_0 (ATerm t);
+ATerm try_1_0 (ATerm e_93 (ATerm), ATerm t);
+ATerm topdown_1_0 (ATerm b_95 (ATerm), ATerm t);
+ATerm xtc_io_transform_1_0 (ATerm c_5 (ATerm), ATerm t);
 ATerm pass_verbose_0_0 (ATerm t);
 ATerm xtc_transform_2_0 (ATerm l_6 (ATerm), ATerm m_6 (ATerm), ATerm t);
 ATerm if_verbose3_1_0 (ATerm w_118 (ATerm), ATerm t);
-static ATerm a_0 (ATerm t);
+static ATerm c_0 (ATerm t);
 ATerm pass_v_verbose_0_0 (ATerm t);
 ATerm xtc_find_0_0 (ATerm t);
 static ATerm d_0 (ATerm t);
-ATerm xtc_sglr_1_0 (ATerm e_0 (ATerm), ATerm t);
+ATerm xtc_sglr_1_0 (ATerm c_7 (ATerm), ATerm t);
 ATerm xtc_io_wrap_1_0 (ATerm r_5 (ATerm), ATerm t);
-static ATerm g_0 (ATerm t);
+static ATerm f_0 (ATerm t);
 static ATerm h_0 (ATerm t);
-static ATerm i_0 (ATerm t);
+static ATerm m_0 (ATerm t);
+static ATerm n_0 (ATerm t);
+static ATerm o_0 (ATerm t);
+static ATerm p_0 (ATerm t);
 ATerm main_0_0 (ATerm t);
 ATerm main_0 (ATerm t)
 {
@@ -176,59 +192,90 @@ ATerm main_0 (ATerm t)
   t = main_0_0(t);
   return(t);
 }
-static ATerm a_0 (ATerm t)
+static ATerm c_0 (ATerm t)
 {
-  t = (ATerm) ATinsert(ATempty, term_j_0);
+  t = (ATerm) ATinsert(ATempty, term_q_0);
   return(t);
 }
 ATerm pass_v_verbose_0_0 (ATerm t)
 {
   t = (ATerm) ATempty;
-  t = if_verbose3_1_0(a_0, t);
+  t = if_verbose3_1_0(c_0, t);
   return(t);
 }
 static ATerm d_0 (ATerm t)
 {
-  t = term_l_0;
+  t = term_r_0;
   return(t);
 }
-ATerm xtc_sglr_1_0 (ATerm e_0 (ATerm), ATerm t)
+ATerm xtc_sglr_1_0 (ATerm c_7 (ATerm), ATerm t)
 {
-  static ATerm f_0 (ATerm t);
-  static ATerm f_0 (ATerm t)
+  static ATerm e_0 (ATerm t);
+  static ATerm e_0 (ATerm t)
   {
-    ATerm b_0 = NULL,c_0 = NULL;
-    t = term_m_0;
-    t = e_0(t);
+    ATerm b_0 = NULL,g_0 = NULL;
+    t = term_s_0;
+    t = c_7(t);
     t = xtc_find_0_0(t);
     b_0 = t;
-    t = term_m_0;
+    t = term_s_0;
     t = pass_v_verbose_0_0(t);
-    c_0 = t;
-    t = (ATerm) ATinsert(ATinsert(ATinsert(CheckATermList(c_0), b_0), term_o_0), term_n_0);
+    g_0 = t;
+    t = (ATerm) ATinsert(ATinsert(ATinsert(CheckATermList(g_0), b_0), term_u_0), term_t_0);
     return(t);
   }
-  t = xtc_transform_2_0(d_0, f_0, t);
+  t = xtc_transform_2_0(d_0, e_0, t);
   return(t);
 }
-static ATerm g_0 (ATerm t)
+static ATerm f_0 (ATerm t)
 {
   t = xtc_sglr_1_0(h_0, t);
-  t = xtc_transform_2_0(i_0, pass_verbose_0_0, t);
+  t = xtc_transform_2_0(m_0, pass_verbose_0_0, t);
+  t = xtc_io_transform_1_0(n_0, t);
   return(t);
 }
 static ATerm h_0 (ATerm t)
 {
-  t = term_p_0;
+  t = term_v_0;
   return(t);
 }
-static ATerm i_0 (ATerm t)
+static ATerm m_0 (ATerm t)
 {
-  t = term_q_0;
+  t = term_w_0;
+  return(t);
+}
+static ATerm n_0 (ATerm t)
+{
+  t = topdown_1_0(o_0, t);
+  return(t);
+}
+static ATerm o_0 (ATerm t)
+{
+  t = try_1_0(p_0, t);
+  return(t);
+}
+static ATerm p_0 (ATerm t)
+{
+  ATerm i_0 = NULL,j_0 = NULL,k_0 = NULL,l_0 = NULL,a_0 = NULL;
+  l_0 = t;
+  if(match_cons(t, sym_S_1))
+    {
+      j_0 = ATgetArgument(t, 0);
+    }
+  else
+    _fail(t);
+  t = SSLgetAnnotations(l_0);
+  i_0 = t;
+  t = j_0;
+  t = un_double_quote_0_0(t);
+  k_0 = t;
+  t = (ATerm) ATmakeAppl(sym_S_1, k_0);
+  a_0 = t;
+  t = SSLsetAnnotations(a_0, i_0);
   return(t);
 }
 ATerm main_0_0 (ATerm t)
 {
-  t = xtc_io_wrap_1_0(g_0, t);
+  t = xtc_io_wrap_1_0(f_0, t);
   return(t);
 }
--- gpp/xtc-tools/pp-box.c
+++ gpp/xtc-tools/pp-box.c
@@ -7,6 +7,9 @@ static Symbol sym_Nil_0;
 static Symbol sym_Cons_2;
 static Symbol sym_Nil_0;
 static Symbol sym_Cons_2;
+static Symbol sym_Cons_2;
+static Symbol sym_Nil_0;
+static Symbol sym_S_1;
 static void init_module_constructors (void)
 {
   sym_Nil_0 = ATmakeSymbol("Nil", 0, ATfalse);
@@ -17,35 +20,48 @@ static void init_module_constructors (void)
   ATprotectSymbol(sym_Nil_0);
   sym_Cons_2 = ATmakeSymbol("Cons", 2, ATfalse);
   ATprotectSymbol(sym_Cons_2);
+  sym_Cons_2 = ATmakeSymbol("Cons", 2, ATfalse);
+  ATprotectSymbol(sym_Cons_2);
+  sym_Nil_0 = ATmakeSymbol("Nil", 0, ATfalse);
+  ATprotectSymbol(sym_Nil_0);
+  sym_S_1 = ATmakeSymbol("S", 1, ATfalse);
+  ATprotectSymbol(sym_S_1);
 }
-static ATerm term_m_0;
-static ATerm term_l_0;
-static ATerm term_j_0;
-static ATerm term_i_0;
-static ATerm term_g_0;
+static ATerm term_s_0;
+static ATerm term_r_0;
+static ATerm term_q_0;
+static ATerm term_p_0;
+static ATerm term_o_0;
 static void init_module_constant_terms (void)
 {
-  ATprotect(&(term_g_0));
-  term_g_0 = (ATerm) ATmakeAppl(ATmakeSymbol("ast2abox", 0, ATtrue));
-  ATprotect(&(term_i_0));
-  term_i_0 = (ATerm) ATmakeAppl(ATmakeSymbol("Box.pp.af", 0, ATtrue));
-  ATprotect(&(term_j_0));
-  term_j_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-p", 0, ATtrue));
-  ATprotect(&(term_l_0));
-  term_l_0 = (ATerm) ATmakeAppl(ATmakeSymbol("abox-format", 0, ATtrue));
-  ATprotect(&(term_m_0));
-  term_m_0 = (ATerm) ATmakeAppl(ATmakeSymbol("abox2text", 0, ATtrue));
+  ATprotect(&(term_o_0));
+  term_o_0 = (ATerm) ATmakeAppl(ATmakeSymbol("ast2abox", 0, ATtrue));
+  ATprotect(&(term_p_0));
+  term_p_0 = (ATerm) ATmakeAppl(ATmakeSymbol("Box.pp.af", 0, ATtrue));
+  ATprotect(&(term_q_0));
+  term_q_0 = (ATerm) ATmakeAppl(ATmakeSymbol("-p", 0, ATtrue));
+  ATprotect(&(term_r_0));
+  term_r_0 = (ATerm) ATmakeAppl(ATmakeSymbol("abox-format", 0, ATtrue));
+  ATprotect(&(term_s_0));
+  term_s_0 = (ATerm) ATmakeAppl(ATmakeSymbol("abox2text", 0, ATtrue));
 }
 #include <srts/init-stratego-application.h>
 ATerm pass_verbose_0_0 (ATerm t);
 ATerm xtc_transform_2_0 (ATerm l_6 (ATerm), ATerm m_6 (ATerm), ATerm t);
 ATerm xtc_find_0_0 (ATerm t);
+ATerm double_quote_0_0 (ATerm t);
+ATerm try_1_0 (ATerm e_93 (ATerm), ATerm t);
+ATerm topdown_1_0 (ATerm b_95 (ATerm), ATerm t);
+ATerm xtc_io_transform_1_0 (ATerm c_5 (ATerm), ATerm t);
 ATerm xtc_io_wrap_1_0 (ATerm r_5 (ATerm), ATerm t);
-static ATerm b_0 (ATerm t);
 static ATerm c_0 (ATerm t);
 static ATerm d_0 (ATerm t);
 static ATerm e_0 (ATerm t);
-static ATerm f_0 (ATerm t);
+static ATerm j_0 (ATerm t);
+static ATerm k_0 (ATerm t);
+static ATerm l_0 (ATerm t);
+static ATerm m_0 (ATerm t);
+static ATerm n_0 (ATerm t);
 ATerm io_pp_box_0_0 (ATerm t);
 ATerm main_0_0 (ATerm t);
 ATerm main_0 (ATerm t)
@@ -54,40 +70,71 @@ ATerm main_0 (ATerm t)
   t = main_0_0(t);
   return(t);
 }
-static ATerm b_0 (ATerm t)
-{
-  t = xtc_transform_2_0(c_0, d_0, t);
-  t = xtc_transform_2_0(e_0, pass_verbose_0_0, t);
-  t = xtc_transform_2_0(f_0, pass_verbose_0_0, t);
-  return(t);
-}
 static ATerm c_0 (ATerm t)
 {
-  t = term_g_0;
+  t = xtc_io_transform_1_0(d_0, t);
+  t = xtc_transform_2_0(k_0, l_0, t);
+  t = xtc_transform_2_0(m_0, pass_verbose_0_0, t);
+  t = xtc_transform_2_0(n_0, pass_verbose_0_0, t);
   return(t);
 }
 static ATerm d_0 (ATerm t)
 {
-  ATerm a_0 = NULL;
-  t = term_i_0;
-  t = xtc_find_0_0(t);
-  a_0 = t;
-  t = (ATerm) ATinsert(ATinsert(ATempty, a_0), term_j_0);
+  t = topdown_1_0(e_0, t);
   return(t);
 }
 static ATerm e_0 (ATerm t)
 {
-  t = term_l_0;
+  t = try_1_0(j_0, t);
   return(t);
 }
-static ATerm f_0 (ATerm t)
+static ATerm j_0 (ATerm t)
 {
-  t = term_m_0;
+  ATerm a_0 = NULL,f_0 = NULL,g_0 = NULL,h_0 = NULL,b_0 = NULL;
+  h_0 = t;
+  if(match_cons(t, sym_S_1))
+    {
+      f_0 = ATgetArgument(t, 0);
+    }
+  else
+    _fail(t);
+  t = SSLgetAnnotations(h_0);
+  a_0 = t;
+  t = f_0;
+  t = double_quote_0_0(t);
+  g_0 = t;
+  t = (ATerm) ATmakeAppl(sym_S_1, g_0);
+  b_0 = t;
+  t = SSLsetAnnotations(b_0, a_0);
+  return(t);
+}
+static ATerm k_0 (ATerm t)
+{
+  t = term_o_0;
+  return(t);
+}
+static ATerm l_0 (ATerm t)
+{
+  ATerm i_0 = NULL;
+  t = term_p_0;
+  t = xtc_find_0_0(t);
+  i_0 = t;
+  t = (ATerm) ATinsert(ATinsert(ATempty, i_0), term_q_0);
+  return(t);
+}
+static ATerm m_0 (ATerm t)
+{
+  t = term_r_0;
+  return(t);
+}
+static ATerm n_0 (ATerm t)
+{
+  t = term_s_0;
   return(t);
 }
 ATerm io_pp_box_0_0 (ATerm t)
 {
-  t = xtc_io_wrap_1_0(b_0, t);
+  t = xtc_io_wrap_1_0(c_0, t);
   return(t);
 }
 ATerm main_0_0 (ATerm t)
