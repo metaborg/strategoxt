4965b54e58c7c16800cbc4cb3ba4dfff8c8079cb
Martin Bravenboer <martin.bravenboer@gmail.com>
2005-01-30 17:00:55 +0000

    * xtc-tools/ast2abox.str: pass the verbose option to ast2abox.
    	* trans/2abox/Ast-2-abox.str: added lots of comments and indenting
    	to make the code more clear for outsiders.
    
    	* trans/2abox/Ast-2-abox.str: fixed an issue with tuples (or any
    	selector) in separated lists. This fixes STR-82.
    
    	* tests/ast2text-tests.str: added a (badly needed) testsuite for
    	the combination of ast2abox and abox2text.
    
    svn path=/trunk/StrategoXT/; revision=8860

---
+++ gpp/tests/DataType.pp
@@ -0,0 +1,6 @@
+[
+  DataType -- H [KW["data"] _1 KW["="] _2],
+  DataType.2:iter-sep -- H [_1 KW["|"]],
+  DataType.2:iter-sep.1:seq -- H hs=1[_1 _2],
+  DataType.2:iter-sep.1:seq.2:iter-star -- _1
+]
