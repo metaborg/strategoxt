a86da55ed2a132383f87df319f7e1c604e5b0f88
Martin Bravenboer <martin.bravenboer@gmail.com>
2005-02-12 22:46:07 +0000

    * analyse/parse-unit/tests/*: implemented a badly needed testsuite 	for parse-unit.
    svn path=/trunk/StrategoXT/; revision=9016

---
+++ sdf-tools/analyse/parse-unit/tests/parse-unit-checks.sh
@@ -0,0 +1,60 @@
+#! /bin/sh
+
+FAIL=0
+PASS=0
+REPORT=""
+IFS=''
+
+incfail() {
+  FAIL=$(($FAIL+1))
+}
+
+incpass() {
+  PASS=$(($PASS+1))
+}
+
+appendreport() {
+  REPORT="$REPORT\n$line"
+}
+
+testsuccess() {
+  ${PARSE_UNIT} -i $suite.testsuite -p Exp.tbl --verbose $VERBOSE
+  if test $? -eq 1; then
+    incfail
+    line="* FAIL: testsuite $suite failed (should succeed)" appendreport
+  else
+    incpass
+    line="* PASS: testsuite $suite succeeded (and should succeed)" appendreport
+  fi
+}
+
+testfail() {
+  ${PARSE_UNIT} -i $suite.testsuite -p Exp.tbl --verbose $VERBOSE
+  if test $? -eq 0; then
+    incfail
+    line="* FAIL: testsuite $suite succeeded (should fail)" appendreport
+  else
+    incpass
+    line="* PASS: testsuite $suite failed (and should fail)" appendreport
+  fi  
+}
+
+suite=failure-1      testfail
+suite=failure-2      testfail
+suite=larger-failure testfail
+suite=success        testsuccess
+
+echo "===================================================================="
+echo "                        Parse-unit checks                           "
+echo "===================================================================="
+echo -e "$REPORT"
+echo ""
+echo "===================================================================="
+echo "FAIL: $FAIL"
+echo "PASS: $PASS"
+echo "===================================================================="
+if test $FAIL != "0"; then
+  exit 1
+else
+  exit 0
+fi
