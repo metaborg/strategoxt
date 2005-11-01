703212756c82c2b3c6b7c23b895b42ff653de470
Martin Bravenboer <martin.bravenboer@gmail.com>
2005-11-01 15:21:34 +0000

    STR-430
    svn path=/trunk/StrategoXT/; revision=13270

--- srts/src/aterm-extension.c
+++ srts/src/aterm-extension.c
@@ -51,7 +51,7 @@ ATermList ATmap(ATermList l, ATerm (* f)(ATerm))
     {
       ATerm hd = f(ATgetFirst(l));
       if(hd == NULL) return NULL;
-      ATerm tl = ATmap(ATgetNext(l), f);
+      ATermList tl = ATmap(ATgetNext(l), f);
       if(tl == NULL) return NULL;
       return ATinsert(tl, hd);
     }
