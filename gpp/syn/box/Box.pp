805c0a603fbaf303be134ee110e61dd5384ef8bf
Martin Bravenboer <martin.bravenboer@gmail.com>
2007-01-14 20:50:25 +0000

    A    gpp/test/Box/box.testsuite
    Syntax definitions should have testsuites.
    
    A    gpp/test/StrategoBox/preterm.testsuite
    
    Embeddings should have testsuites.
    
    M    gpp/syn/*/*.sdf
    
    Dropped the use of aliases.
    Dropped GPL copyright claims.
    
    
    svn path=/strategoxt/trunk/; revision=16435

--- gpp/syn/box/Box.pp
+++ gpp/syn/box/Box.pp
@@ -1,15 +1,54 @@
-[ SOpt -- _ 1 KW [ "=" ] _ 2 , SOptB -- _ 1 KW [ "=" ] _ 2 , VS -- KW [ "vs" ] ,
-HS -- KW [ "hs" ] , IS -- KW [ "is" ] , H -- KW [ "H" ] _ 1 KW [ "[" ] _ 2 KW [
-"]" ] , V -- KW [ "V" ] _ 1 KW [ "[" ] _ 2 KW [ "]" ] , HV -- KW [ "HV" ] _ 1 KW
-[ "[" ] _ 2 KW [ "]" ] , ALT -- KW [ "ALT" ] KW [ "[" ] _ 1 _ 2 KW [ "]" ] , A
--- KW [ "A" ] _ 1 _ 2 KW [ "[" ] _ 3 KW [ "]" ] , R -- KW [ "R" ] _ 1 KW [ "[" ]
-_ 2 KW [ "]" ] , AL -- KW [ "l" ] _ 1 , AC -- KW [ "c" ] _ 1 , AR -- KW [ "r" ]
-_ 1 , AOPTIONS -- KW [ "(" ] _ 1 KW [ ")" ] , AOPTIONS . 1 : iter-star-sep -- _
-1 KW [ "," ] , FNAT -- _ 1 KW [ "=" ] _ 2 , FFID -- _ 1 KW [ "=" ] _ 2 , F -- KW
-[ "F" ] _ 1 , FBOX -- _ 1 KW [ "[" ] _ 2 KW [ "]" ] , FN -- KW [ "fn" ] , FM --
-KW [ "fm" ] , SE -- KW [ "se" ] , SH -- KW [ "sh" ] , SZ -- KW [ "sz" ] , CL --
-KW [ "cl" ] , KW -- KW [ "KW" ] , VAR -- KW [ "VAR" ] , NUM -- KW [ "NUM" ] ,
-MATH -- KW [ "MATH" ] , LBL -- KW [ "LBL" ] KW [ "[" ] _ 1 _ 2 KW [ "]" ] , REF
--- KW [ "REF" ] KW [ "[" ] _ 1 _ 2 KW [ "]" ] , C -- KW [ "C" ] _ 1 KW [ "[" ] _
-2 KW [ "]" ] , L -- KW [ "L" ] KW [ "[" ] _ 1 _ 2 KW [ "]" ] , LNAT -- KW [ "L"
-] KW [ "[" ] _ 1 _ 2 KW [ "]" ] , S -- _ 1 ]
+[
+   SOpt                     -- _1 KW["="] _2,
+   SOptB                    -- _1 KW["="] _2,
+   VS                       -- KW["vs"],
+   HS                       -- KW["hs"],
+   IS                       -- KW["is"],
+   H                        -- V  [V vs=2 [KW["H"] _1] V vs=2 [KW["["] _2] KW["]"]],
+   H.1:iter-star            -- _1,
+   H.2:iter-star            -- _1,
+   V                        -- V  [V vs=2 [KW["V"] _1] V vs=2 [KW["["] _2] KW["]"]],
+   V.1:iter-star            -- _1,
+   V.2:iter-star            -- _1,
+   HV                       -- V  [V vs=2 [KW["HV"] _1] V vs=2 [KW["["] _2] KW["]"]],
+   HV.1:iter-star           -- _1,
+   HV.2:iter-star           -- _1,
+   ALT                      -- KW["ALT"] KW["["] _1 _2 KW["]"],
+   A                        -- KW["A"] _1 _2 KW["["] _3 KW["]"],
+   A.2:iter-star            -- _1,
+   A.3:iter-star            -- _1,
+   R                        -- V  [V vs=2 [KW["R"] _1] V vs=2 [KW["["] _2] KW["]"]],
+   R.1:iter-star            -- _1,
+   R.2:iter-star            -- _1,
+   AL                       -- V  [H  [KW["l"]] _1],
+   AL.1:iter-star           -- _1,
+   AC                       -- V  [H  [KW["c"]] _1],
+   AC.1:iter-star           -- _1,
+   AR                       -- V  [H  [KW["r"]] _1],
+   AR.1:iter-star           -- _1,
+   AOPTIONS                 -- KW["("] _1 KW[")"],
+   AOPTIONS.1:iter-star-sep -- _1 KW[","],
+   FNAT                     -- _1 KW["="] _2,
+   FFID                     -- _1 KW["="] _2,
+   F                        -- V  [H  [KW["F"]] _1],
+   F.1:iter-star            -- _1,
+   FBOX                     -- _1 KW["["] _2 KW["]"],
+   FN                       -- KW["fn"],
+   FM                       -- KW["fm"],
+   SE                       -- KW["se"],
+   SH                       -- KW["sh"],
+   SZ                       -- KW["sz"],
+   CL                       -- KW["cl"],
+   KW                       -- KW["KW"],
+   VAR                      -- KW["VAR"],
+   NUM                      -- KW["NUM"],
+   MATH                     -- KW["MATH"],
+   LBL                      -- KW["LBL"] KW["["] _1 _2 KW["]"],
+   REF                      -- KW["REF"] KW["["] _1 _2 KW["]"],
+   C                        -- V  [V vs=2 [KW["C"] _1] V vs=2 [KW["["] _2] KW["]"]],
+   C.1:iter-star            -- _1,
+   C.2:iter-star            -- _1,
+   L                        -- KW["L"] KW["["] _1 _2 KW["]"],
+   LNAT                     -- KW["L"] KW["["] _1 _2 KW["]"],
+   S                        -- _1
+]
