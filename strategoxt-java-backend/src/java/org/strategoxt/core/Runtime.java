ab6ae416884f873dedf7c242c2d7720f8077fd7d
Karl Trygve Kalleberg <karltk@gmail.com>
2007-12-08 22:15:48 +0000

    Can now compile simple build expressions, and the result can be compiled with javac and executed.
    svn path=/strc-java/trunk/; revision=17385

--- strategoxt-java-backend/src/java/org/strategoxt/core/Runtime.java
+++ strategoxt-java-backend/src/java/org/strategoxt/core/Runtime.java
@@ -4,7 +4,7 @@ import aterm.pure.PureFactory;
 import aterm.ATerm;
 
 public class Runtime {
-	public static PureFactory atermFactory; 
+	public static PureFactory atermFactory = new PureFactory(); 
 	
 	public static ATerm prepareCLIArgs(String[] args) { return atermFactory.makeList(); }
 }
