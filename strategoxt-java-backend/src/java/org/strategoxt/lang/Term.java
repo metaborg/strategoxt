e96862e1538537a0d60c35459e563c15b4bcc5a9
Karl Trygve Kalleberg <karltk@gmail.com>
2008-03-05 18:50:07 +0000

    More runtime support skeleton code.
    svn path=/strc-java/trunk/; revision=17527

--- strategoxt-java-backend/src/java/org/strategoxt/lang/Term.java
+++ strategoxt-java-backend/src/java/org/strategoxt/lang/Term.java
@@ -1,8 +1,10 @@
 package org.strategoxt.lang;
 
 import org.strategoxt.lang.terms.IStrategoTerm;
+import org.strategoxt.lang.terms.IStrategoList;
 
 public class Term {
 	
 	public static boolean nonNull(IStrategoTerm t) { return t != null; }
+	public static IStrategoList checkTermList(IStrategoList ls) { return ls; }
 }
