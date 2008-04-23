3dcd9c4b1470de1bd56fe238fa04bf3957029f20
Karl Trygve Kalleberg <karltk@gmail.com>
2008-04-23 18:26:42 +0000

    Made into an Eclipse Java project for easier compilation; started on support for compiler language tests.
    svn path=/strc-java/trunk/; revision=17776

---
+++ strategoxt-java-backend/src/java/org/strategoxt/compiler/tests/LanguageTestSuite.java
@@ -0,0 +1,37 @@
+package org.strategoxt.compiler.tests;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import aterm.ATerm;
+import aterm.ATermFactory;
+import aterm.pure.PureFactory;
+
+import junit.framework.TestCase;
+
+public class LanguageTestSuite extends TestCase {
+
+	protected void expect(String testFile, String from, String to) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+		
+		Class<?> c = cl.loadClass("org.strategoxt.compiler.tests.cases." + testFile);
+		Method m = c.getMethod("testMain", String.class);
+		
+		Object s = m.invoke(null, from);
+		ATermFactory af = new PureFactory();
+		System.out.println("result = " + s);
+		ATerm got = af.parse((String)s);
+		ATerm wanted = af.parse(to);
+		assertTrue(got.equals(wanted));
+	}
+	
+	protected void fail(String testFile, String from) throws ClassNotFoundException, SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+		ClassLoader cl = ClassLoader.getSystemClassLoader();
+		
+		Class<?> c = cl.loadClass(testFile);
+		Method m = c.getMethod("testFail", String.class, String.class);
+		
+		Object s = m.invoke(null, from);
+		
+	}
+}
