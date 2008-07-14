00d14d4fe235f3bae6844218687e6ed1f0b8e1c8
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2008-07-14 15:36:41 +0000

    Add "dyn" and "call" implementation. - Use Blob with destructor to remove unused dynamic closures. - Verify the number of arguments. - Extend the "call" primitive to handle dynamic strategies   in addition to the strategies declared with DYNAMIC_CALLS.
    svn path=/strategoxt/branches/strategoxt-heap-closure/; revision=18027

--- stratego-libraries/runtime/lib/srts/stratego-dynamic-call.h
+++ stratego-libraries/runtime/lib/srts/stratego-dynamic-call.h
@@ -7,6 +7,7 @@ extern ATermTable strategy_table ;
 void SRTS_register_function(ATerm name, StrCL closure);
 StrCL SRTS_lookup_function(ATerm name);
 
-ATerm SRTS_dyn(StrSL sl, StrCL cl, ATerm ns, ATerm nt, ATerm t);
+ATerm SRTS_dyn(StrCL cl, unsigned int ns, unsigned int nt);
+StrCL SRTS_call(ATerm s, unsigned int ns, unsigned int nt);
 
 #endif
--- stratego-libraries/runtime/lib/stratego-dynamic-call.c
+++ stratego-libraries/runtime/lib/stratego-dynamic-call.c
@@ -1,5 +1,24 @@
 #include "srts/stratego.h"
 
+static ATbool dynclosure_destructor(ATermBlob b);
+
+static void init_module_constant_terms(void) {}
+static void init_module_constructors(void) {
+  ATregisterBlobDestructor(dynclosure_destructor);
+}
+
+#include "srts/init-stratego-module.h"
+#define DYNC_MAGIC 0x44796e43 /* DynC */
+
+typedef struct str_dynclosure *StrDL;
+
+struct str_dynclosure
+{
+  unsigned int magic;
+  struct str_closure cl;
+  unsigned int ns, nt;
+};
+
 ATermTable strategy_table ;
 
 void SRTS_register_function(ATerm name, StrCL fcn) {
@@ -17,7 +36,59 @@ StrCL SRTS_lookup_function(ATerm name) {
   return (StrCL) ATgetBlobData((ATermBlob) res);
 }
 
-ATerm SRTS_dyn(StrSL sl, StrCL cl, ATerm ns, ATerm nt, ATerm t)
-{
-  return t;
+ATerm SRTS_dyn(StrCL cl, unsigned int ns, unsigned int nt) {
+  StrDL dl;
+
+  dl = malloc(sizeof(struct str_dynclosure));
+  if (NULL == dl)
+  {
+    fprintf(stderr, "*** STRS_dyn: not enough memory.\n");
+    return NULL;
+  }
+
+  dl->magic = DYNC_MAGIC;
+  dl->cl = *cl;
+  dl->ns = ns;
+  dl->nt = nt;
+
+  return (ATerm) ATmakeBlob(sizeof(struct str_dynclosure), dl);
+}
+
+static StrDL Blob2DynClosure(ATerm b) {
+  StrDL dl;
+
+  if(NULL == b || !ATisBlob(b) || !ATgetBlobSize(b) != sizeof(struct str_dynclosure))
+    return NULL;
+
+  dl = (StrDL) ATgetBlobData((ATermBlob) b);
+  if(dl->magic != DYNC_MAGIC)
+    return NULL;
+
+  return dl;
+}
+
+StrCL SRTS_call(ATerm s, unsigned int ns, unsigned int nt) {
+  StrDL dl = Blob2DynClosure(s);
+
+  if(NULL == dl)
+    return NULL;
+
+  if(dl->ns != ns || dl->nt != nt)
+  {
+    fprintf(stderr, "*** SRTS_call: wrong number of arguments, excepts %u strategies and %u terms.\n", dl->ns, dl->nt);
+    return NULL;
+  }
+
+  return &dl->cl;
+}
+
+static ATbool dynclosure_destructor(ATermBlob b) {
+  StrDL dl = Blob2DynClosure((ATerm) b);
+
+  if(NULL == dl)
+    return ATfalse;
+
+  free(dl);
+
+  return ATtrue;
 }
