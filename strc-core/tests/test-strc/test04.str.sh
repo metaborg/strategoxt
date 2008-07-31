83a5a74a73dee21a0a69c71ca3e04a0940b0c747
Nicolas B. Pierron <nicolas.b.pierron@nbp.name>
2008-07-31 09:19:13 +0000

    Revert extra options to by pass libtool behaviour.
    svn path=/strategoxt/branches/strategoxt-fix-compilation/; revision=18075

--- strc-core/tests/test-strc/test04.str.sh
+++ strc-core/tests/test-strc/test04.str.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-${STRC} -i "${SRCDIR}/test04.str" -o test04 -la-la ${STRATEGO_LIB}/lib/libstratego-lib || exit 1
+${STRC} -i "${SRCDIR}/test04.str" -o test04 -la ${STRATEGO_LIB}/lib/libstratego-lib.la || exit 1
 
 ./test04 || exit 1 
--- strc-core/tests/test-strc/test09.str.sh
+++ strc-core/tests/test-strc/test09.str.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -x
 
 # Compile a library
-${STRC} -i "${SRCDIR}/test09-lib.str" -o-la libtest09-lib --library -la stratego-lib || exit 1
+${STRC} -i "${SRCDIR}/test09-lib.str" -o libtest09-lib.la --library -la stratego-lib || exit 1
 
 # Link with the library
-${STRC} -i "${SRCDIR}/test09.str" -o test09 -la-la libtest09-lib -I ./ || exit 1
+${STRC} -i "${SRCDIR}/test09.str" -o test09 -la libtest09-lib.la -I ./ || exit 1
 
 # Run the program
 ./test09 || exit 1
