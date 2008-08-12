775d1b65b614ba8dcba15c0f9053a086ba130482
Lennart Kats <lclkats@gmail.com>
2008-08-12 08:17:38 +0000

    Tweaked the message displayed by "with"
    svn path=/strategoxt/branches/strategoxt-with/; revision=18119

--- strc-core/tests/test-strc/test13.str.sh
+++ strc-core/tests/test-strc/test13.str.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-${STRC} -i test13.str -la stratego-lib || exit 1 
+${STRC} -i test13.str -la stratego-lib -O 0|| exit 1 
 
 rm -f test13.str.actual
-./test13 2>&1 | head -n 3 > test13.str.actual
+./test13 > test13.str.actual 2>&1
 
 diff test13.str.expected test13.str.actual || (echo "Result not expected; test failed"; exit 1)
