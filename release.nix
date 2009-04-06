785f52e1d65c6a503db2da3ff30bde612dc1b4e6
Rob Vermaas <rob.vermaas@gmail.com>
2009-04-06 13:18:47 +0000

    release.nix for hydra
    svn path=/strategoxt/trunk/; revision=18747

---
+++ release.nix
@@ -0,0 +1,78 @@
+{ nixpkgs ? ../nixpkgs
+}:
+
+let
+
+  pkgs = import nixpkgs { };
+
+  strategoxtBaseline =
+    pkgs.stdenv.mkDerivation {
+      name = "strategoxt-baseline";
+      src = pkgs.fetchurl {
+              url = "http://releases.strategoxt.org/strategoxt/strategoxt-0.17pre18222-xi157j0s/strategoxt-0.17pre18222.tar.gz";
+              sha256 = "1d8cd6bc0d24281edef302434277f74dbe5ccac8493e410af5cce9cef5ec43d4";
+            };
+
+      buildInputs = defaultBuildInputs pkgs ;
+    };
+
+    defaultBuildInputs = pkgs :
+      [ pkgs.pkgconfig
+        pkgs.aterm
+        pkgs.strategoPackages017.sdf
+        pkgs.getopt
+      ];
+
+    defaultSvnBuildInputs = pkgs :
+      [ pkgs.autoconf
+        pkgs.automake19x
+        pkgs.libtool
+      ] ++ defaultBuildInputs pkgs;
+
+  makeStrategoXTTarball = name : strategoxtSrc : strategoxtBaseline : officialRelease :  
+      pkgs.releaseTools.makeSourceTarball {
+      name = name;
+      version = builtins.readFile ./version;
+
+      src = strategoxtSrc;
+
+      configureFlags = " --with-strategoxt=${strategoxtBaseline} --enable-werror";
+
+      buildInputs = defaultSvnBuildInputs pkgs ;
+
+      doCheck = false;
+      dontInstall = false;
+      useTempPrefix = false;
+      tarballs = "*.tar.gz";
+
+      inherit officialRelease;
+    };
+
+  jobs = rec {
+
+    tarball =
+      { strategoxtSrc ? {outPath = ./. ; rev = 1234;}
+      , officialRelease ? false
+      }: 
+      makeStrategoXTTarball "strategoxt-tarball" strategoxtSrc strategoxtBaseline officialRelease ;
+
+    build =
+      { tarball ? jobs.tarball {}
+      , system ? "i686-linux"
+      }:
+ 
+      with import nixpkgs {inherit system;};
+
+      releaseTools.nixBuild {
+        name = "strategoxt";
+        src = tarball;
+ 
+        buildInputs = defaultSvnBuildInputs pkgs ;
+
+      };
+
+  } ;
+
+  
+in jobs
+
