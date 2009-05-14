49055cdd804e0bfccdffd4f03a16ae6eca948e93
Lennart Kats <lclkats@gmail.com>
2009-05-14 14:11:50 +0000

    Work on jsglr parsing from strj.
    svn path=/strc-java/trunk/; revision=19418

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/Context.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/Context.java
@@ -3,6 +3,7 @@ package org.strategoxt.lang;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.spoofax.interpreter.adapter.aterm.BAFBasicTermFactory;
 import org.spoofax.interpreter.core.InterpreterException;
 import org.spoofax.interpreter.core.InterpreterExit;
 import org.spoofax.interpreter.core.StackTracer;
@@ -14,7 +15,6 @@ import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.strategoxt.lang.compat.CompatManager;
-import org.strategoxt.lang.terms.BAFTermFactory;
 
 /**
  * The runtime context of a compiled Stratego strategy.
@@ -37,7 +37,7 @@ public class Context extends StackTracer {
 	private final ITermFactory factory;
     
     public Context() {
-    	this(new BAFTermFactory());
+    	this(new BAFBasicTermFactory());
     }
     
     public Context(ITermFactory factory) {
--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/sglr/JSGLR_parse_string_pt_compat.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/sglr/JSGLR_parse_string_pt_compat.java
@@ -1,7 +1,12 @@
 package org.strategoxt.lang.compat.sglr;
 
 import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.library.jsglr.JSGLR_parse_string_pt;
+import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.jsglr.Disambiguator;
 import org.spoofax.jsglr.ParseTable;
 import org.spoofax.jsglr.SGLR;
@@ -11,20 +16,29 @@ import aterm.ATermFactory;
 /**
  * @author Lennart Kats <lennart add lclnet.nl>
  */
-public class JSGLR_parse_string_pt_compat extends JSGLR_parse_string_pt {
+public class JSGLR_parse_string_pt_compat extends AbstractPrimitive {
 	
 	private final Disambiguator filterSettings;
+	
+	private final JSGLR_parse_string_pt parser;
 
 	protected JSGLR_parse_string_pt_compat(WrappedATermFactory termFactory, Disambiguator filterSettings) {
-		super(termFactory);
+		super("JSGLR_parse_string_pt_compat", 0, 0);
+		
 		this.filterSettings = filterSettings;
+		parser = new JSGLR_parse_string_pt(termFactory) {
+			@Override
+			protected SGLR makeSGLR(ATermFactory factory, ParseTable table) {
+				SGLR result = new SGLR(factory, table);
+				result.setDisambiguator(JSGLR_parse_string_pt_compat.this.filterSettings);
+				return result;		
+			}
+		};
 	}
-	
+
 	@Override
-	protected SGLR makeSGLR(ATermFactory factory, ParseTable table) {
-		SGLR result = new SGLR(factory, table);
-		result.setDisambiguator(filterSettings);
-		return result;		
+	public boolean call(IContext env, Strategy[] svars, IStrategoTerm[] tvars) throws InterpreterException {
+		return parser.call(env, svars, tvars);
 	}
 
 }
