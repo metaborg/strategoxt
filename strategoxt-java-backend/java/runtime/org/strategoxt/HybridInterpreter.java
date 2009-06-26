8e09c36adfd77aa927e3bbdb1f15a95d3f02591d
Lennart Kats <lclkats@gmail.com>
2009-06-26 07:59:17 +0000

    Actual HybridInterpreter class.
    svn path=/strc-java/trunk/; revision=19560

---
+++ strategoxt-java-backend/java/runtime/org/strategoxt/HybridInterpreter.java
@@ -0,0 +1,52 @@
+package org.strategoxt;
+
+import org.spoofax.interpreter.adapter.aterm.BAFBasicTermFactory;
+import org.spoofax.interpreter.core.IContext;
+import org.spoofax.interpreter.core.Interpreter;
+import org.spoofax.interpreter.terms.ITermFactory;
+import org.strategoxt.lang.Context;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class HybridInterpreter extends Interpreter {
+	
+	private final Context compiledContext;
+
+	public HybridInterpreter() {
+		this(new BAFBasicTermFactory());
+	}
+
+	public HybridInterpreter(ITermFactory factory) {
+		this(factory, factory);
+	}
+
+	public HybridInterpreter(ITermFactory termFactory, ITermFactory programFactory) {
+		super(termFactory, programFactory);
+		
+		compiledContext = new Context(termFactory);
+		registerLibraries();
+	}
+	
+	protected void registerLibraries() {
+		IContext context = getContext();
+		Context compiledContext = getCompiledContext();
+		
+		// TODO: Optimize - could this be shared across threads?
+		libstratego_aterm.registerInterop(context, compiledContext);
+		libstratego_lib.registerInterop(context, compiledContext);
+		libstratego_rtg.registerInterop(context, compiledContext);
+		libstratego_sdf.registerInterop(context, compiledContext);
+		libstratego_xtc.registerInterop(context, compiledContext);
+		libstratego_sglr.registerInterop(context, compiledContext);
+		libstratego_tool_doc.registerInterop(context, compiledContext);
+		libstratego_rtg.registerInterop(context, compiledContext);
+		libjava_front.registerInterop(context, compiledContext);
+		libstrc.registerInterop(context, compiledContext);
+	}
+	
+	public Context getCompiledContext() {
+		return compiledContext;
+	}
+
+}
