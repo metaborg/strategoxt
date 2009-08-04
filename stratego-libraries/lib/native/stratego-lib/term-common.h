f5bb5ca3cab2b5d8a2b2ee21bf7134fb95c6e8b3
Lennart Kats <lclkats@gmail.com>
2009-08-04 11:56:08 +0000

    Added a new branch that generates one Java file for each strategy.
    svn path=/strc-java/branches/one-file-per-strategy/; revision=19805

--- stratego-libraries/lib/native/stratego-lib/term-common.h
+++
@@ -1,9 +0,0 @@
-
-ATerm SSL_mkterm(ATerm, ATerm) ;
-ATerm SSL_explode_term(ATerm) ;
-ATerm SSL_get_constructor(ATerm) ;
-ATerm SSL_get_arguments(ATerm) ;
-ATerm SSL_get_appl_arguments_map(StrCL, ATerm) ;
-ATerm SSL_address_lt(ATerm, ATerm) ;
-ATerm SSL_address(ATerm) ;
-ATerm SSL_checksum(ATerm) ;
--- strc-core/tests/test-strc/test05.str.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-${STRC} -i test05.str -CD "Macro()=((ATerm) ATmakeInt(2))" || exit 1
-
-./test05 || exit 1 
- 
