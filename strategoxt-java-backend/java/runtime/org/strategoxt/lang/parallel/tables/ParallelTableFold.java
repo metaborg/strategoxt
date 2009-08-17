c034a96249b28dd5ce3230692115601935c31b44
Lennart Kats <lclkats@gmail.com>
2009-08-17 14:41:15 +0000

    More parallel table stub classes.
    svn path=/strc-java/trunk/; revision=19836

---
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/parallel/tables/ParallelTableFold.java
@@ -0,0 +1,10 @@
+package org.strategoxt.lang.parallel.tables;
+
+import org.spoofax.interpreter.library.ssl.SSL_table_fold;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class ParallelTableFold extends SSL_table_fold {
+
+}
---
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/parallel/tables/ParallelTableValuesFold.java
@@ -0,0 +1,10 @@
+package org.strategoxt.lang.parallel.tables;
+
+import org.spoofax.interpreter.library.ssl.SSL_table_values_fold;
+
+/**
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class ParallelTableValuesFold extends SSL_table_values_fold {
+	// TODO
+}
