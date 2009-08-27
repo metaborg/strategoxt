b25813ab6c79327c828a2b08a640291479b99553
Lennart Kats <lclkats@gmail.com>
2009-08-27 15:15:31 +0000

    Added new if-java-platform and if-c-platform strategies for some degree of conditional compilation.
    svn path=/strc-java/trunk/; revision=19881

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
@@ -1,15 +1,10 @@
 package org.strategoxt.lang.compat;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
 import org.spoofax.interpreter.library.jsglr.JSGLRLibrary;
-import org.spoofax.interpreter.library.ssl.SSLLibrary;
-import org.spoofax.interpreter.library.ssl.StrategoHashMap;
-import org.spoofax.interpreter.terms.IStrategoTerm;
-import org.spoofax.interpreter.terms.ITermFactory;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.compat.override.jsglr_parser.jsglr_parser;
 import org.strategoxt.lang.compat.override.jsglr_parser_compat.jsglr_parser_compat;
@@ -57,7 +52,6 @@ public class CompatManager {
 		if ("libstratego_lib".equals(component)) {
 			context.addOperatorRegistry(new CompatLibrary());
 			performance_tweaks.init(context);
-			initConfig();
 			xtc_compat.init(context); // also deals with native calls for libstratego-lib
 		} else if ("libstratego_sglr".equals(component)) {
 			WrappedATermFactory atermFactory = new WrappedATermFactory();
@@ -65,30 +59,6 @@ public class CompatManager {
 			context.addOperatorRegistry(new SGLRCompatLibrary(atermFactory));
 			jsglr_parser.init(context);
 			jsglr_parser_compat.init(context);
-		}
-	}
-
-	private void initConfig() {
-		ITermFactory factory = context.getFactory();
-		Map<IStrategoTerm, IStrategoTerm> config = getConfigTable();
-		
-		config.put(factory.makeString("JAVA_PLATFORM"), factory.makeInt(1));
-	}
-
-	private StrategoHashMap getConfigTable() {
-		ITermFactory factory = context.getFactory();
-		SSLLibrary library = (SSLLibrary) context.getOperatorRegistry(SSLLibrary.REGISTRY_NAME);
-		if (library == null) throw new IllegalStateException("Standard operator registry not found");
-		StrategoHashMap allTables = library.getTableTable();
-		
-		IStrategoTerm configRef = allTables.get(factory.makeString("config"));
-		if (configRef == null) {
-			StrategoHashMap result = new StrategoHashMap(117, 75);
-			configRef = factory.makeAppl(factory.makeConstructor("Hashtable", 1), result);
-			allTables.put(factory.makeString("config"), configRef);
-			return result;
-		} else {
-			return (StrategoHashMap) configRef.getSubterm(0);
 		}
 	}
 }
