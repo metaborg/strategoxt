8d9d5d88e4669a5b819a5d321d816dec42c553b6
Lennart Kats <lclkats@gmail.com>
2009-09-12 10:11:51 +0000

    Tweaks to errors and warnings.
    svn path=/strc-java/trunk/; revision=19978

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/Strategy.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/Strategy.java
@@ -1,5 +1,7 @@
 package org.strategoxt.lang;
 
+import java.util.ArrayList;
+
 import org.spoofax.interpreter.terms.IStrategoTerm;
 
 /**
@@ -75,151 +77,155 @@ public class Strategy {
 					case 5: return invoke(context, current, s[0], s[1], s[2], s[3], t[0], t[1], t[2], t[3], t[4]);
 				}
 		}
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		
+		ArrayList<Object> args = new ArrayList<Object>();
+		for (Object o : s) args.add(o);
+		for (Object o : t) args.add(o);
+		throw new IllegalArgumentException(args);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException();
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(t1, t2, t3, t4, t5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, t1, t2, t3, t4, t5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, t1, t2, t3, t4, t5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, t1, t2, t3, t4, t5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, t1, t2, t3, t4, t5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5, IStrategoTerm t1) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5, t1);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5, IStrategoTerm t1, IStrategoTerm t2) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5, t1, t2);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5, t1, t2, t3);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5, t1, t2, t3, t4);
 	}
 
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, Strategy s1, Strategy s2, Strategy s3, Strategy s4, Strategy s5, IStrategoTerm t1, IStrategoTerm t2, IStrategoTerm t3, IStrategoTerm t4, IStrategoTerm t5) {
-		throw new IllegalArgumentException("Illegal arguments for " + getName());
+		throw new IllegalArgumentException(s1, s2, s3, s4, s5, t1, t2, t3, t4, t5);
 	}
 	
 	public String getName() {
@@ -229,5 +235,45 @@ public class Strategy {
 	@Override
 	public String toString() {
 		return getName();
+	}
+
+	private static String argsToText(Object... args) {
+		if (args.length == 0)
+			return " (none)";
+		
+		StringBuilder result = new StringBuilder(" (");
+		boolean printedBar = false;
+
+		if (args[0] instanceof IStrategoTerm) {
+			result.append('|');
+			printedBar = true;
+		}
+		result.append(args[0]);
+		
+		for (int i = 1; i < args.length; i++) {
+			Object arg = args[0];
+			if (!printedBar && arg instanceof IStrategoTerm) {
+				result.append(" |");
+				printedBar = true;
+			} else {
+				result.append(", ");
+			}
+			result.append(arg);
+		}
+		result.append(")");
+		
+		return result.toString();
+	}
+
+	/**
+	 * Thrown to indicate that a strategy has been passed an illegal or
+	 * inappropriate argument or arguments.
+	 */
+	private class IllegalArgumentException extends java.lang.IllegalArgumentException {
+		private static final long serialVersionUID = 121251125123L;
+		
+		public IllegalArgumentException(Object... args) {
+			super("Illegal arguments for " + getName() + " " + argsToText(args));
+		}
 	}
 }
