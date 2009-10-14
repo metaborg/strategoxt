de1a336f604969ec05d739487b0f3c419c58ebb6
Lennart Kats <lclkats@gmail.com>
2009-10-14 14:00:30 +0000

    - Add dollars to capital letters in generated Java files for our Windows/Mac friends - Support passing id/fail as parametrized strategy arguments
    svn path=/strc-java/trunk/; revision=20090

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/_Fail.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/_Fail.java
@@ -5,7 +5,7 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 /**
  * @author Lennart Kats <lennart add lclnet.nl>
  */
-public class _Fail extends Strategy {
+public class _Fail extends DynamicStrategy {
 	public static final _Fail instance = new _Fail();
 	
 	private _Fail() {
@@ -14,6 +14,11 @@ public class _Fail extends Strategy {
 	
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+		return null;
+	}
+	
+	@Override
+	public IStrategoTerm invokeDynamic(Context context, IStrategoTerm current, Strategy[] sargs, IStrategoTerm[] targs) {
 		return null;
 	}
 }
--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/_Id.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/_Id.java
@@ -5,7 +5,7 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 /**
  * @author Lennart Kats <lennart add lclnet.nl>
  */
-public class _Id extends Strategy {
+public class _Id extends DynamicStrategy {
 	public static final _Id instance = new _Id();
 	
 	private _Id() {
@@ -14,6 +14,11 @@ public class _Id extends Strategy {
 
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
+		return current;
+	}
+	
+	@Override
+	public IStrategoTerm invokeDynamic(Context context, IStrategoTerm current, Strategy[] sargs, IStrategoTerm[] targs) {
 		return current;
 	}
 }
