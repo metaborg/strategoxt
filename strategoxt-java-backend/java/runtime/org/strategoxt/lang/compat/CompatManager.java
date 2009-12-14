3247fa428d90cbc6a6dd04072e24d7b5bf4ea6b6
Lennart Kats <lclkats@gmail.com>
2009-12-14 20:23:43 +0000

    Fixed position information for files parsed from Stratego (SPI-24)
    svn path=/strc-java/trunk/; revision=20399

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
@@ -33,8 +33,8 @@ public class CompatManager {
 
 	public void registerComponent(String component) {
 		synchronized (asyncComponents) {
-			asyncComponents.add(component);
-			activateComponent(component);
+			if (asyncComponents.add(component))
+				activateComponent(component);
 		}
 	}
 	
