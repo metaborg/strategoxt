e7ec584b466c0e3e588cf86a2dd0cbcc93d2301b
Nathan Bruning <nathan.bruning@gmail.com>
2010-05-04 15:18:08 +0000

    Add ANT property provider for strategoxt.jar classpath
    svn path=/strc-java/trunk/; revision=20830

---
+++ strategoxt-java-backend/java/eclipse/org/strategoxt/eclipse/ant/StrategoJarAntPropertyProvider.java
@@ -0,0 +1,28 @@
+package org.strategoxt.eclipse.ant;
+
+import java.io.File;
+
+import org.eclipse.ant.core.IAntPropertyValueProvider;
+import org.eclipse.core.runtime.Platform;
+
+/**
+ * Copied from org.strategoxt.imp.metatooling.StrategoJarAntPropertyProvider
+ * @author Lennart Kats <lennart add lclnet.nl>
+ */
+public class StrategoJarAntPropertyProvider implements IAntPropertyValueProvider {
+	
+	public String getAntPropertyValue(String antPropertyName) {
+		String result = org.strategoxt.stratego_lib.Main.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+		if (Platform.getOS().equals(Platform.OS_WIN32)) {
+			// FIXME: proper paths on Windows
+			result = result.substring(1);
+		}
+		if (!result.endsWith(".jar")) { // ensure correct jar at development time
+			String result2 = result + "/../strategoxt.jar";
+			if (new File(result2).exists()) return result2;
+			result2 = result + "/java/strategoxt.jar";
+			if (new File(result2).exists()) return result2;
+		}
+		return result;
+	}
+}
