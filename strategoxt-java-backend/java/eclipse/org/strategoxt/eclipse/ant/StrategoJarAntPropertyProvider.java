211ef6ada358eab63178a0aa414165dda2676dfb
Lennart Kats <lclkats@gmail.com>
2011-02-24 12:18:04 +0000

    svn path=/strategoxt-java-backend/trunk/; revision=22140

--- strategoxt-java-backend/java/eclipse/org/strategoxt/eclipse/ant/StrategoJarAntPropertyProvider.java
+++ strategoxt-java-backend/java/eclipse/org/strategoxt/eclipse/ant/StrategoJarAntPropertyProvider.java
@@ -6,12 +6,15 @@ import org.eclipse.ant.core.IAntPropertyValueProvider;
 import org.eclipse.core.runtime.Platform;
 
 /**
- * Copied from org.strategoxt.imp.metatooling.StrategoJarAntPropertyProvider
  * @author Lennart Kats <lennart add lclnet.nl>
  */
 public class StrategoJarAntPropertyProvider implements IAntPropertyValueProvider {
 	
 	public String getAntPropertyValue(String antPropertyName) {
+		return getStrategoJarPath();
+	}
+
+	public static String getStrategoJarPath() {
 		String result = org.strategoxt.stratego_lib.Main.class.getProtectionDomain().getCodeSource().getLocation().getFile();
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
 			// FIXME: proper paths on Windows
