689890e9ee83fe84312a953dc9db072830d1d6aa
Rob Vermaas <rob.vermaas@gmail.com>
2011-06-10 12:24:56 +0000

    svn path=/strategoxt/branches/xml-front-java/; revision=23041

--- strc-core/tests/test-strc/test04.str.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-${STRC} -i test04.str -la ${STRATEGO_LIB}/lib/libstratego-lib.la || exit 1
-
-./test04 || exit 1 
--- strc-core/tests/test-strc/test14.str.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-${STRC} -i test14.str -la stratego-lib || exit 1 
-
-rm -f test14.str.actual
-./test14 2>&1 | tail -n 2 > test14.str.actual
-
-diff test14.str.expected test14.str.actual || (echo "Result not expected; test failed"; exit 1)
