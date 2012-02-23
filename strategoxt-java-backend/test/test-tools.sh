16729374d320eb6bdb16bc9881a922b680e03500
Lennart Kats <lclkats@gmail.com>
2012-02-23 14:16:46 +0000

    added a hack to support naming annotations in org.strategoxt.tools.pack-sdf. I'm not happy with this solution, but it'll make the annotations available for use in Spoofax faster.
    svn path=/strategoxt-java-backend/trunk/; revision=24380

--- strategoxt-java-backend/test/test-tools.sh
+++ strategoxt-java-backend/test/test-tools.sh
@@ -5,3 +5,10 @@ java -cp $CLASSPATH start org.strategoxt.tools.main-sdf2rtg -i strc1/Expressions
 java -cp $CLASSPATH start org.strategoxt.tools.main-rtg2sig --module Expressions > /dev/null
 
 java -cp $CLASSPATH start org.strategoxt.tools.main-pack-sdf -i strc1/Expressions.sdf > /dev/null
+
+java -cp $CLASSPATH start org.strategoxt.tools.main-pack-sdf -i sdf-ext/Test.sdf > sdf-ext/Test-filtered.sdf
+
+if grep -E "@|NAM" sdf-ext/Test-filtered.sdf; then
+  echo Illegal output from pack-sdf
+  exit 1
+fi
