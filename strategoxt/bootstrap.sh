f06ce8cd701ae63097dbd0c2ec257eb1747c7afd
Vlad Vergu <v.vergu@gmail.com>
2013-01-29 21:55:40 +0100

    * New spoofax-libs * Clearer bootstrap shell script

--- strategoxt/bootstrap.sh
+++ strategoxt/bootstrap.sh
@@ -1,7 +1,40 @@
 #! /bin/sh
-BASE_COMPILER=base/strategoxt.jar
+COMPILER0="base/strategoxt.jar.base"
+COMPILER1="base/strategoxt.jar.1"
+COMPILER2="base/strategoxt.jar.2"
+COMPILER3="base/strategoxt.jar.3"
 
-cp $BASE_COMPILER .
-echo "Build 1/3" && ./build.sh && 
-echo "Building 2/3" && ant clean && ./build.sh && 
-echo "Building 3/3" && ant clean && ./build.sh 
+BASE_PATH=`pwd`
+
+echo -e "Build 1/3\n" &&
+ant clean &&
+git clean -Xdf &&
+cd syntax/java-front &&
+svn st | grep '^?' | awk '{print $2}' | xargs rm -rf &&
+cd $BASE_PATH &&
+rm -f strategoxt-base.jar &&
+ln -s $COMPILER0 strategoxt-base.jar &&
+./build.sh &&
+cp strategoxt.jar $COMPILER1 &&
+
+echo -e "Build 2/3\n" &&
+ant clean &&
+git clean -Xdf &&
+cd syntax/java-front &&
+svn st | grep '^?' | awk '{print $2}' | xargs rm -rf &&
+cd $BASE_PATH &&
+rm -f strategoxt-base.jar &&
+ln -s $COMPILER1 strategoxt-base.jar &&
+./build.sh &&
+cp strategoxt.jar $COMPILER2 &&
+
+echo -e "Build 3/3\n" &&
+ant clean &&
+git clean -Xdf &&
+cd syntax/java-front &&
+svn st | grep '^?' | awk '{print $2}' | xargs rm -rf &&
+cd $BASE_PATH &&
+rm -f strategoxt-base.jar &&
+ln -s $COMPILER2 strategoxt-base.jar &&
+./build.sh &&
+cp strategoxt.jar $COMPILER3
