a296bb131268b9cda57ad73aaff6b6754b321614
guwac <g.h.wachsmuth@tudelft.nl>
2013-03-28 21:48:02 +0100

    garbage collection

--- org.metaborg.runtime.task/src/org/metaborg/runtime/task/Tasks.java
+++ org.metaborg.runtime.task/src/org/metaborg/runtime/task/Tasks.java
@@ -1,6 +1,5 @@
 package org.metaborg.runtime.task;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -13,35 +12,44 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.IStrategoTuple;
 import org.spoofax.interpreter.terms.ITermFactory;
 
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.Multimap;
+import static com.google.common.collect.Sets.filter;
+
+import static com.google.common.base.Predicates.in;
+import static com.google.common.base.Predicates.not;
 
 public class Tasks {
-	private final ITermFactory factory;
-	private final ManyToManyMap<IStrategoInt, IStrategoString> toPartition = ManyToManyMap.create();
-	private final ManyToManyMap<IStrategoInt, IStrategoInt> toDependency = ManyToManyMap.create();
-	private final Map<IStrategoInt, IStrategoTerm> toInstruction = new ConcurrentHashMap<IStrategoInt, IStrategoTerm>();
-	private final Map<IStrategoInt, IStrategoList> toResult = new ConcurrentHashMap<IStrategoInt, IStrategoList>();
 	
-	private final Set<IStrategoInt> initialTasks = new HashSet<IStrategoInt>();
+	private final ITermFactory factory;
+	
+	private final ManyToManyMap<IStrategoInt, IStrategoString> toPartition = ManyToManyMap.create();
+	private final ManyToManyMap<IStrategoInt, IStrategoInt> toDependency   = ManyToManyMap.create();
+	private final Map<IStrategoInt, IStrategoTerm> toInstruction = new ConcurrentHashMap<IStrategoInt, IStrategoTerm>();
+	private final Map<IStrategoInt, IStrategoList> toResult      = new ConcurrentHashMap<IStrategoInt, IStrategoList>();
+	
 	private final Set<IStrategoInt> addedTasks = new HashSet<IStrategoInt>();
+	private final Set<IStrategoInt> removedTasks = new HashSet<IStrategoInt>();
+	
+	private final Set<IStrategoInt> tasks    = toInstruction.keySet();
+	private final Set<IStrategoInt> garbage  = filter(tasks, not(in(toPartition.keys())));
+	private final Set<IStrategoInt> solved   = toResult.keySet();
+	private final Set<IStrategoInt> stalled  = toDependency.keySet();
+	// private final Set<IStrategoInt> solvable = filter(tasks, not(or(in(solved), in(stalled))));
 	
 	public Tasks(ITermFactory factory) {
 		this.factory = factory;
 	}
 	
-	public IStrategoInt addTask(IStrategoTerm instruction, IStrategoString partition, IStrategoList dependencies) {
+	public IStrategoInt addTask(IStrategoString partition, IStrategoList dependencies, IStrategoTerm instruction) {
 		
 		IStrategoInt taskID = factory.makeInt(instruction.hashCode());
 		if(toInstruction.put(taskID, instruction) == null)
 			addedTasks.add(taskID);
-		initialTasks.remove(taskID);
+		removedTasks.remove(taskID);
 		
 		toPartition.put(taskID, partition);
 		
         while(!dependencies.isEmpty()) {
-        	IStrategoInt head = (IStrategoInt) dependencies.head();
-            toDependency.put(taskID, head);
+        	toDependency.put(taskID, (IStrategoInt) dependencies.head());
             dependencies = dependencies.tail();
         }
         
@@ -58,23 +66,33 @@ public class Tasks {
 	
 	public void startAnalysis(IStrategoString partition) {
 		addedTasks.clear();
-		initialTasks.clear();
-		initialTasks.addAll(toPartition.getInverse(partition));
+		removedTasks.clear();
+		removedTasks.addAll(toPartition.getInverse(partition));
 	}
 	
 	public IStrategoTuple stopAnalysis(IStrategoString partition) {
-		for(IStrategoInt removed : initialTasks)
-			removeTask(removed, partition);
+
+		for(IStrategoInt removed : removedTasks)
+			toPartition.remove(removed, partition);
 		
-		return factory.makeTuple(factory.makeList(initialTasks), factory.makeList(addedTasks));
+		collectGarbage();
+		
+		return factory.makeTuple(factory.makeList(addedTasks), factory.makeList(removedTasks));
 	}
 	
-	private void removeTask(IStrategoInt taskID, IStrategoString partition) {
-		toPartition.remove(taskID, partition);
-		if(!toPartition.containsKey(taskID)) {
-			toInstruction.remove(taskID);
-			toDependency.removeAll(taskID);
-			toResult.remove(taskID);
-		}
+//	private void removeTask(IStrategoInt taskID, IStrategoString partition) {
+//		toPartition.remove(taskID, partition);
+//		
+//		if(!toPartition.containsKey(taskID)) {
+//			toDependency.removeAll(taskID);
+//			toInstruction.remove(taskID);
+//			toResult.remove(taskID);
+//		}
+//	}
+	
+	private void collectGarbage() {
+		tasks.removeAll(garbage);
+		solved.removeAll(garbage);
+		stalled.removeAll(garbage);
 	}
-}
+ }
