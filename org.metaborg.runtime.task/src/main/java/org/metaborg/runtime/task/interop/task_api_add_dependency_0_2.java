45e2002368332f746d1d5a7605e5e5ed82bd450f
Gabriël Konat <gabrielkonat@gmail.com>
2013-05-14 10:52:19 +0200

    Wrong class name.

--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_add_dependency_0_2.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_add_dependency_0_2.java
@@ -5,8 +5,8 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
 
-public class task_api_add_dependency_0_3 extends Strategy {
-	public static task_api_add_dependency_0_3 instance = new task_api_add_dependency_0_3();
+public class task_api_add_dependency_0_2 extends Strategy {
+	public static task_api_add_dependency_0_2 instance = new task_api_add_dependency_0_2();
 
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm taskID,
