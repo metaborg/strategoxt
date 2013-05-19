6dd90787a3c9e1abd1a7da98f2c64085411f6963
Vlad Vergu <v.vergu@gmail.com>
2013-05-19 13:58:46 +0200

    Minor improvements to shell scripts for local building

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib .
+ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib strategoxt-base.jar
