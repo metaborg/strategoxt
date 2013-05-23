b608830e42b826223a93aedbfd001a1c2bda6a89
Vlad Vergu <v.vergu@gmail.com>
2013-05-23 18:51:18 +0200

    Change primitives to use the getWrappedFactory() method

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/typesmart/UnsafeBuild.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/typesmart/UnsafeBuild.java
@@ -10,45 +10,43 @@ import org.spoofax.terms.StrategoList;
 import org.spoofax.terms.Term;
 
 /**
- * Builds an application term using the given term factory. The constructor name
- * is expected as a term argument, the argument list is expected as the current
- * term.
+ * Builds an application term using the given term factory. The constructor name is expected as a
+ * term argument, the argument list is expected as the current term.
  * 
  * @author seba
  */
 public class UnsafeBuild extends AbstractPrimitive {
 
-	private ITermFactory factory;
+    private ITermFactory factory;
 
-	public UnsafeBuild() {
-		super("SUGARJ_unsafe_build", 0, 1);
+    public UnsafeBuild() {
+	super("SUGARJ_unsafe_build", 0, 1);
+    }
+
+    @Override
+    public boolean call(IContext context, Strategy[] svars, IStrategoTerm[] tvars)
+	    throws InterpreterException {
+	if (factory == null) {
+	    ITermFactory smartFactory = context.getFactory();
+	    if (smartFactory instanceof TypesmartTermFactory) {
+		factory = ((TypesmartTermFactory) smartFactory).getWrappedFactory();
+	    } else {
+		factory = smartFactory;
+	    }
 	}
 
-	@Override
-	public boolean call(IContext context, Strategy[] svars,
-			IStrategoTerm[] tvars) throws InterpreterException {
-		if (factory == null) {
-			ITermFactory smartFactory = context.getFactory();
-			if (smartFactory instanceof TypesmartTermFactory) {
-				factory = ((TypesmartTermFactory) smartFactory)
-						.getBaseFactory();
-			} else {
-				factory = smartFactory;
-			}
-		}
+	String ctr = Term.javaString(tvars[0]);
 
-		String ctr = Term.javaString(tvars[0]);
+	if (!(context.current() instanceof StrategoList))
+	    return false;
 
-		if (!(context.current() instanceof StrategoList))
-			return false;
+	StrategoList args = (StrategoList) context.current();
 
-		StrategoList args = (StrategoList) context.current();
+	IStrategoTerm[] argArray = args.getAllSubterms();
+	IStrategoTerm unsafeTerm = factory.makeAppl(factory.makeConstructor(ctr, argArray.length),
+		argArray);
 
-		IStrategoTerm[] argArray = args.getAllSubterms();
-		IStrategoTerm unsafeTerm = factory.makeAppl(
-				factory.makeConstructor(ctr, argArray.length), argArray);
-
-		context.setCurrent(unsafeTerm);
-		return true;
-	}
+	context.setCurrent(unsafeTerm);
+	return true;
+    }
 }
