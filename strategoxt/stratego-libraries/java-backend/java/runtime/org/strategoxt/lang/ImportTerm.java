b985860165f1ca31501ee788ef0ce33375942fae
Vlad Vergu <v.vergu@gmail.com>
2013-05-29 00:48:27 +0200

    Force the ImportTerm to eagerly evaluate

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/ImportTerm.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/ImportTerm.java
@@ -16,25 +16,50 @@ import org.spoofax.terms.io.binary.TermReader;
  * @author Lennart Kats <lennart add lclnet.nl>
  */
 public class ImportTerm extends LazyTerm {
-	
-	private final ITermFactory factory;
-	
+
+	private ITermFactory factory;
+
 	private final String path, name;
-	
+
 	private final Class<?> container;
-	
+
+	private Throwable eagerError;
+	private IStrategoTerm eagerResult;
+
 	private ZipFile lastZipFile;
-	
+
 	public ImportTerm(ITermFactory factory, Class<?> container, String path, String name) {
 		assert path.endsWith("/");
 		this.factory = factory;
 		this.container = container;
 		this.path = path;
 		this.name = name;
+		eagerInit();
 	}
-	
+
+	public void eagerInit() {
+		long now = System.currentTimeMillis();
+		try{
+			eagerResult = init();
+		}catch(Throwable t){
+			eagerError = t;
+		}finally{
+			this.factory = null;
+		}
+		long later = System.currentTimeMillis();
+		System.err.println("Eager import term: " + name + " duration: " + (later - now) + " ms");
+	}
+
 	@Override
 	protected IStrategoTerm init() {
+		if(eagerResult != null){
+			IStrategoTerm tmp = eagerResult;
+			eagerResult = null;
+			return tmp;
+		}
+		if(eagerError != null){
+			throw new StrategoException("Eager import-term failed", eagerError);
+		}
 		InputStream stream = openStream();
 		try {
 			IStrategoTerm result = new TermReader(factory).parseFromStream(stream);
@@ -55,6 +80,7 @@ public class ImportTerm extends LazyTerm {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			this.factory = null;
 		}
 	}
 
@@ -73,8 +99,8 @@ public class ImportTerm extends LazyTerm {
 	}
 
 	/**
-	 * Attempt to open resources in JARs using the Java ZipFile API,
-	 * hopefully avoiding JVM classloader crashes.
+	 * Attempt to open resources in JARs using the Java ZipFile API, hopefully avoiding JVM
+	 * classloader crashes.
 	 */
 	private InputStream tryOpenStreamFromJar() {
 		try {
@@ -83,9 +109,12 @@ public class ImportTerm extends LazyTerm {
 				File jarFile = new File(location.toURI());
 				ZipFile jar = lastZipFile = new ZipFile(jarFile);
 				ZipEntry entry = jar.getEntry(removePrecedingSlash(path) + name);
-				if (entry == null) entry = jar.getEntry(path + name);
-				if (entry == null) entry = jar.getEntry(name);
-				if (entry == null) return null;
+				if (entry == null)
+					entry = jar.getEntry(path + name);
+				if (entry == null)
+					entry = jar.getEntry(name);
+				if (entry == null)
+					return null;
 				return jar.getInputStream(entry);
 			} else {
 				return null;
@@ -94,7 +123,7 @@ public class ImportTerm extends LazyTerm {
 			return null; // be forgiving
 		}
 	}
-	
+
 	private static String removePrecedingSlash(String path) {
 		return path.startsWith("/") ? path.substring(1) : path;
 	}
