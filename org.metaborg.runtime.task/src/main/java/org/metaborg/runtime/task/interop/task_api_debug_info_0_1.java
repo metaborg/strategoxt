59147807b7b5bdeba88f72618b4dababc9745cfd
Guido Wachsmuth <g.h.wachsmuth@tudelft.nl>
2013-06-08 12:33:47 +0200

    fixed erroneous merge in Java code

--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_debug_info_0_1.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_debug_info_0_1.java
@@ -4,10 +4,11 @@ import java.util.Collection;
 
 import org.metaborg.runtime.task.TaskEngine;
 import org.metaborg.runtime.task.TaskManager;
-import org.spoofax.interpreter.terms.IStrategoInt;
+import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
+import org.spoofax.interpreter.terms.IStrategoTuple;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
@@ -16,24 +17,34 @@ public class task_api_debug_info_0_1 extends Strategy {
 	public static task_api_debug_info_0_1 instance = new task_api_debug_info_0_1();
 
 	@Override
-	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm partition) {
+	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm partitionOrID) {
 		final ITermFactory factory = context.getFactory();
 		final TaskEngine engine = TaskManager.getInstance().getCurrent();
-		Collection<IStrategoInt> taskIDs = engine.getInPartition((IStrategoString) partition);
+		
+		if(Tools.isTermString(partitionOrID)) {
+			Collection<IStrategoTerm> taskIDs = engine.getInPartition((IStrategoString) partitionOrID);
+			IStrategoList list = factory.makeList();
+			for(IStrategoTerm taskID : taskIDs) {
+				list = factory.makeListCons(createDebugTuple(taskID, engine, factory), list);
+			}
 
-		IStrategoList list = factory.makeList();
-		for(IStrategoInt taskID : taskIDs) {
-			IStrategoTerm instruction = engine.getInstruction(taskID);
-			IStrategoTerm message = engine.getMessage(taskID);
-			IStrategoTerm result =
-				engine.hasFailed(taskID) ? factory.makeAppl(factory.makeConstructor("Fail", 0)) : engine
-					.getResult(taskID);
-			list =
-				factory.makeListCons(factory.makeTuple(taskID, instruction,
-					result == null ? factory.makeAppl(factory.makeConstructor("None", 0)) : result, message == null
-						? factory.makeAppl(factory.makeConstructor("None", 0)) : message), list);
+			return list;
+		} else if(Tools.isTermInt(partitionOrID)) {
+			return createDebugTuple(partitionOrID, engine, factory);
 		}
+		
+		return null;
+	}
 
-		return list;
+	private IStrategoTuple createDebugTuple(IStrategoTerm taskID, TaskEngine engine, ITermFactory factory) {
+		IStrategoTerm instruction = engine.getInstruction(taskID);
+		IStrategoList dependencies = factory.makeList(engine.getDependencies(taskID));
+		IStrategoTerm message = engine.getMessage(taskID);
+		IStrategoTerm result =
+			engine.hasFailed(taskID) ? factory.makeAppl(factory.makeConstructor("Fail", 0)) : engine.getResult(taskID);
+
+		return factory.makeTuple(taskID, instruction, dependencies,
+			result == null ? factory.makeAppl(factory.makeConstructor("None", 0)) : result,
+			message == null ? factory.makeAppl(factory.makeConstructor("None", 0)) : message);
 	}
 }
--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_setup_0_1.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/interop/task_api_setup_0_1.java
@@ -1,5 +1,7 @@
 package org.metaborg.runtime.task.interop;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.metaborg.runtime.task.TaskManager;
 import org.spoofax.interpreter.core.Tools;
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -11,8 +13,12 @@ public class task_api_setup_0_1 extends Strategy {
 
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current, IStrategoTerm projectPath) {
-		TaskManager.getInstance().loadTaskEngine(Tools.asJavaString(projectPath), context.getFactory(),
-			context.getIOAgent());
-		return current;
+		try {
+			TaskManager.getInstance().loadTaskEngine(Tools.asJavaString(projectPath), context.getFactory(),
+				context.getIOAgent());
+			return current;
+		} catch(NoSuchAlgorithmException e) {
+			return null;
+		}
 	}
 }
