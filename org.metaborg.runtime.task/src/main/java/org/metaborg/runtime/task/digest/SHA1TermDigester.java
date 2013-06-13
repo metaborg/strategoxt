c4e00577e15bd33dc7fb49d419084637e4a720ac
Gabriël Konat <gabrielkonat@gmail.com>
2013-06-12 23:18:39 -0700

    Fix SHA1 term digester.

--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/digest/SHA1TermDigester.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/digest/SHA1TermDigester.java
@@ -3,7 +3,9 @@ package org.metaborg.runtime.task.digest;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.WeakHashMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.spoofax.interpreter.terms.IStrategoAppl;
 import org.spoofax.interpreter.terms.IStrategoInt;
@@ -13,47 +15,61 @@ import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.IStrategoTuple;
 import org.spoofax.interpreter.terms.ITermFactory;
+import org.spoofax.terms.attachments.TermAttachmentSerializer;
+import org.spoofax.terms.io.binary.SAFWriter;
 
 public class SHA1TermDigester implements ITermDigester {
 	private final MessageDigest digest;
+	private final Map<IStrategoTerm, IStrategoTerm> mapping = new HashMap<IStrategoTerm, IStrategoTerm>();
 	private final ByteBuffer intBuffer = ByteBuffer.allocate(4);
 	private final ByteBuffer doubleBuffer = ByteBuffer.allocate(8);
-	private final WeakHashMap<IStrategoTerm, IStrategoTerm> cache = new WeakHashMap<IStrategoTerm, IStrategoTerm>();
 
 	public SHA1TermDigester() throws NoSuchAlgorithmException {
 		digest = MessageDigest.getInstance("SHA-1");
 	}
 
 	public IStrategoTerm digest(IStrategoTerm term, ITermFactory factory) {
-		IStrategoTerm digested = cache.get(term);
-		if(digested == null) {
-			digestTop(term);
+		IStrategoTerm digestedTerm = mapping.get(term);
+		if(digestedTerm == null) {
+			digest.reset();
+			final TermAttachmentSerializer serializer = new TermAttachmentSerializer(factory);
+			digestTopSerializer(serializer.toAnnotations(term));
 			byte[] data = digest.digest();
-			digested = factory.makeTuple(factory.makeInt(toInt(data, 0)), factory.makeInt(toInt(data, 4)));
-			cache.put(term, digested);
+			digestedTerm = factory.makeTuple(factory.makeInt(toInt(data, 0)), factory.makeInt(toInt(data, 4)));
+			mapping.put(term, digestedTerm);
 		}
-
-		return digested;
+		return digestedTerm;
 	}
 
 	public void undigest(IStrategoTerm term) {
-		
+		mapping.remove(term);
 	}
-	
+
 	public boolean digested(IStrategoTerm term) {
-		return true;
+		return mapping.containsKey(term);
 	}
-	
+
 	public IStrategoTerm state(ITermFactory factory) {
-		return factory.makeInt(0);
+		IStrategoList mappingTerm = factory.makeList();
+		for (Entry<IStrategoTerm, IStrategoTerm> entry : mapping.entrySet())
+			mappingTerm = factory.makeListCons(
+					factory.makeTuple(entry.getKey(), entry.getValue()),
+					mappingTerm);
+		return mappingTerm;
 	}
-	
+
 	public void setState(IStrategoTerm state) {
-		
+		mapping.clear();
+		for (IStrategoTerm entry : state.getSubterm(1))
+			mapping.put(entry.getSubterm(0), entry.getSubterm(1));
 	}
-	
+
 	public void reset() {
-		cache.clear();
+		mapping.clear();
+	}
+
+	private void digestTopSerializer(IStrategoTerm term) {
+		digest.update(SAFWriter.writeTermToSAFString(term));
 	}
 
 	private void digestTop(IStrategoTerm term) {
@@ -63,45 +79,45 @@ public class SHA1TermDigester implements ITermDigester {
 
 	private void digestTerm(IStrategoTerm term) {
 		switch(term.getTermType()) {
-			case IStrategoTerm.APPL: {
-				final IStrategoAppl t = (IStrategoAppl) term;
-				digest.update(t.getConstructor().getName().getBytes());
-				intBuffer.position(0);
-				digest.update(intBuffer.putInt(t.getConstructor().getArity()).array());
+		case IStrategoTerm.APPL: {
+			final IStrategoAppl t = (IStrategoAppl) term;
+			digest.update(t.getConstructor().getName().getBytes());
+			intBuffer.position(0);
+			digest.update(intBuffer.putInt(t.getConstructor().getArity()).array());
 
-				for(IStrategoTerm subterm : t)
-					digestTop(subterm);
-				break;
-			}
-			case IStrategoTerm.TUPLE: {
-				final IStrategoTuple t = (IStrategoTuple) term;
-				for(IStrategoTerm subterm : t)
-					digestTop(subterm);
-				break;
-			}
-			case IStrategoTerm.LIST: {
-				final IStrategoList t = (IStrategoList) term;
-				for(IStrategoTerm subterm : t)
-					digestTop(subterm);
-				break;
-			}
-			case IStrategoTerm.INT: {
-				final IStrategoInt t = (IStrategoInt) term;
-				intBuffer.position(0);
-				digest.update(intBuffer.putInt(t.intValue()).array());
-				break;
-			}
-			case IStrategoTerm.REAL: {
-				final IStrategoReal t = (IStrategoReal) term;
-				doubleBuffer.position(0);
-				digest.update(doubleBuffer.putDouble(t.realValue()).array());
-				break;
-			}
-			case IStrategoTerm.STRING: {
-				final IStrategoString t = (IStrategoString) term;
-				digest.update(t.stringValue().getBytes());
-				break;
-			}
+			for(IStrategoTerm subterm : t)
+				digestTop(subterm);
+			break;
+		}
+		case IStrategoTerm.TUPLE: {
+			final IStrategoTuple t = (IStrategoTuple) term;
+			for(IStrategoTerm subterm : t)
+				digestTop(subterm);
+			break;
+		}
+		case IStrategoTerm.LIST: {
+			final IStrategoList t = (IStrategoList) term;
+			for(IStrategoTerm subterm : t)
+				digestTop(subterm);
+			break;
+		}
+		case IStrategoTerm.INT: {
+			final IStrategoInt t = (IStrategoInt) term;
+			intBuffer.position(0);
+			digest.update(intBuffer.putInt(t.intValue()).array());
+			break;
+		}
+		case IStrategoTerm.REAL: {
+			final IStrategoReal t = (IStrategoReal) term;
+			doubleBuffer.position(0);
+			digest.update(doubleBuffer.putDouble(t.realValue()).array());
+			break;
+		}
+		case IStrategoTerm.STRING: {
+			final IStrategoString t = (IStrategoString) term;
+			digest.update(t.stringValue().getBytes());
+			break;
+		}
 		}
 	}
 
