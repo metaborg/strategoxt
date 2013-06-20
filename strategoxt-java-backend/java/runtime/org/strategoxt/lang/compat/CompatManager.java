16d24f00366eec69f278abfae10e885d33e9d41e
Gabriël Konat <gabrielkonat@gmail.com>
2013-06-19 21:29:49 -0700

    Revert "Merge branch 'feature/task-library'"
    This reverts commit e31c90f17d81cdb65e62c991dc88a95f525ba684, reversing
    changes made to d7af9a5e08a2e5cb5b2be66233b352843ba4db2e.

--- strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
+++ strategoxt-java-backend/java/runtime/org/strategoxt/lang/compat/CompatManager.java
@@ -3,7 +3,6 @@ package org.strategoxt.lang.compat;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.metaborg.runtime.task.primitives.TaskLibrary;
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.VarScope;
 import org.spoofax.interpreter.library.index.IndexLibrary;
@@ -56,7 +55,6 @@ public class CompatManager {
         context.addOperatorRegistry(new OriginLibrary());
         context.addOperatorRegistry(new XMLLibrary());
         context.addOperatorRegistry(new InterpreterLibrary());
-        context.addOperatorRegistry(new TaskLibrary());
 	}
 
 	public void registerComponent(String component) {
