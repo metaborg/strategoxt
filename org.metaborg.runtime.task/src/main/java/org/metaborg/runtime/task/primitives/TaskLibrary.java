7bc3a65cd7da9804d62bc8539a9f7dd6e68c7ea3
Gabriel Konat <gabrielkonat@gmail.com>
2013-06-26 21:29:14 -0700

    Forgot to add primitive to library.

--- org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/primitives/TaskLibrary.java
+++ org.metaborg.runtime.task/src/main/java/org/metaborg/runtime/task/primitives/TaskLibrary.java
@@ -12,6 +12,7 @@ public class TaskLibrary extends AbstractStrategoOperatorRegistry {
 		add(task_api_add_read_0_2.instance);
 		add(task_api_add_task_0_3.instance);
 		add(task_api_becomes_cyclic_0_2.instance);
+		add(task_api_clear_partition_0_1.instance);
 		add(task_api_evaluate_3_0.instance);
 		add(task_api_exists_0_1.instance);
 		add(task_api_get_dependencies_0_1.instance);
