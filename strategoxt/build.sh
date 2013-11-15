40dd3c93936c0499a4969466b023567000ba45ce
Vlad Vergu <v.vergu@gmail.com>
2013-11-15 14:07:40 +0100

    Refuse to try to build if the build environment is not configured

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+: ${STRJ_BASELINE:?"Environment not initialized. Run source PLATFORM-init.sh"}
+: ${STRJ_BASELINE_DEPS:?"Environment not initialized. Run source PLATFORM-init.sh"}
+: ${STRJ_BASELINE_BINS:?"Environment not initialized. Run source PLATFORM-init.sh"}
+: ${STRJ_BASELINE_BASE:?"Environment not initialized. Run source PLATFORM-init.sh"}
+
 ANT_OPTS="-Xss8m -Xmx1024m" ant -Dsdf2bundle=$STRJ_BASELINE -Dnativepath=$STRJ_BASELINE_BINS -lib $STRJ_BASELINE_BASE -lib $STRJ_BASELINE_DEPS $1 || exit 1
