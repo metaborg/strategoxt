31683c1e2e846889439b1a767fb4d2a2248c95cf
Vlad Vergu <v.vergu@gmail.com>
2013-11-22 21:45:53 +0100

    Minor fix in hydra build products. Support multiple builds targets directly in build.sh

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -5,4 +5,4 @@
 : ${STRJ_BASELINE_BINS:?"Environment not initialized. Run source PLATFORM-init.sh"}
 : ${STRJ_BASELINE_BASE:?"Environment not initialized. Run source PLATFORM-init.sh"}
 
-ANT_OPTS="-Xss8m -Xmx1024m" ant -Dsdf2bundle=$STRJ_BASELINE -Dnativepath=$STRJ_BASELINE_BINS -lib $STRJ_BASELINE_BASE -lib $STRJ_BASELINE_DEPS $1 || exit 1
+ANT_OPTS="-Xss8m -Xmx1024m" ant -Dsdf2bundle=$STRJ_BASELINE -Dnativepath=$STRJ_BASELINE_BINS -lib $STRJ_BASELINE_BASE -lib $STRJ_BASELINE_DEPS $@ || exit 1
