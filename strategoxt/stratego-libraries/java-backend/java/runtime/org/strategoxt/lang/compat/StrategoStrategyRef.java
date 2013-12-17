af93e80da609469c34eb01bb5587d1b838a31aac
Vlad Vergu <v.vergu@gmail.com>
2013-12-17 13:50:58 +0100

    Fix incorrect empty annotations passed to StrategoTerm

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/StrategoStrategyRef.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/StrategoStrategyRef.java
@@ -7,6 +7,7 @@ import org.spoofax.interpreter.stratego.Strategy;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.spoofax.terms.StrategoAppl;
+import org.spoofax.terms.TermFactory;
 
 /**
  * @author Vlad Vergu <v.a.vergu add tudelft.nl>
@@ -21,8 +22,7 @@ public class StrategoStrategyRef extends StrategoAppl {
 	private final Strategy sref;
 
 	public StrategoStrategyRef(Strategy sref, ITermFactory factory) {
-		super(factory.makeConstructor("___StrategyReference___", 0), new IStrategoTerm[0], factory
-				.makeList(), factory.getDefaultStorageType());
+		super(factory.makeConstructor("___StrategyReference___", 0), new IStrategoTerm[0], TermFactory.EMPTY_LIST, factory.getDefaultStorageType());
 
 		this.sref = sref;
 	}
