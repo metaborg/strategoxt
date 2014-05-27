1935676e97c33141ea68ffc510923681b46c97ce
Eelco Visser <visser@acm.org>
2014-05-27 09:08:03 +0200

    make scripts work

--- strategoxt/init.sh
+++ strategoxt/init.sh
@@ -1,15 +1,19 @@
 #! /bin/bash
 
 if [ -d "baseline" ]; then
-	echo "A baseline exists already, would you like to update it?"
-	select yn in "Yes" "No"; do
-	    case $yn in
-	        Yes ) ./update-baseline.sh; break;;
-	        No ) break;;
-	    esac
-	done
+  echo "A baseline exists already (to update use update-baseline.sh)"
+	#echo "A baseline exists already, would you like to update it?"
+	#select yn in "Yes" "No"; do
+	#    case $yn in
+	#        "Yes") ./update-baseline.sh; break;;
+	#        "No") break;;
+	#    esac
+	#done
 else
 	./update-baseline.sh
 fi
 
 source init-env.sh $1
+
+echo "Stratego/XT is initialized"
+
--- strategoxt/update-baseline.sh
+++ strategoxt/update-baseline.sh
@@ -2,7 +2,7 @@
 
 rm -rf baseline &&
 
-wget https://github.com/metaborg/strategoxt/releases/download/baseline/strategoxt-baseline.zip &&
+wget --no-check-certificate -O strategoxt-baseline.zip https://github.com/metaborg/strategoxt/releases/download/baseline/strategoxt-baseline.zip &&
 
 unzip strategoxt-baseline.zip &&
 
