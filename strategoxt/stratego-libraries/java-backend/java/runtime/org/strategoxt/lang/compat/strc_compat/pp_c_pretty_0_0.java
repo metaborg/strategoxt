a56b5c5f43a645a042dcb32b0a5d44d12680bda4
Moritz Lichter <lichtemo@googlemail.com>
2014-11-28 13:14:54 +0100

    Only generate imports for -la specified to the compiler, adapted handcoded strategies and switched back to jar for sugarj-common

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/strc_compat/pp_c_pretty_0_0.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/strc_compat/pp_c_pretty_0_0.java
@@ -4,12 +4,21 @@ import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.terms.util.NotImplementedException;
 import org.strategoxt.lang.Context;
 import org.strategoxt.lang.Strategy;
+import org.strategoxt.lang.RegisteringStrategy;
+import org.strategoxt.lang.StrategyCollector;
 
 /**
  * @author Lennart Kats <lennart add lclnet.nl>
  */
-public class pp_c_pretty_0_0 extends Strategy {
+public class pp_c_pretty_0_0 extends RegisteringStrategy {
 	public static pp_c_pretty_0_0 instance = new pp_c_pretty_0_0();
+	
+	public void registerImplementators(StrategyCollector collector) { 
+		collector.registerStrategyImplementator("pp_c_pretty_0_0", instance);
+	}
+	
+	public void bindExecutors(StrategyCollector collector) { 
+	}
 	
 	@Override
 	public IStrategoTerm invoke(Context context, IStrategoTerm current) {
