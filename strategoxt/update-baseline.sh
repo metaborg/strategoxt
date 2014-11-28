b968a5cbce737f69ea4ae755485a85530ede3399
Vlad Vergu <v.vergu@gmail.com>
2014-11-28 11:27:57 +0100

    Add Java-Front as a submodule

--- strategoxt/update-baseline.sh
+++ strategoxt/update-baseline.sh
@@ -12,12 +12,12 @@ tar -xpf strategoxt-distrib.tar &&
 
 chmod a+x share/strategoxt/linux/* share/strategoxt/macosx/* &&
 
-git clone https://github.com/metaborg/java-front.git -b java-bootstrap java-front &&
+# git clone https://github.com/metaborg/java-front.git -b java-bootstrap java-front &&
 
-cd ../syntax  &&
+# cd ../syntax  &&
 
-rm -f java-front &&
+# rm -f java-front &&
 
-ln -s ../baseline/java-front java-front &&
+#ln -s ../baseline/java-front java-front &&
 
 cd ..
