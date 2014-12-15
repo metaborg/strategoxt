67af20e252330a017e4e1b8daa58a7cfce508e63
Gabriel Konat <gabrielkonat@gmail.com>
2014-12-15 15:38:37 +0100

    Compose build using parents instead of parameters. Also build strategoxt JAR.

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -2,7 +2,4 @@
 
 set -eu
 
-BASELINE_VERSION="1.3.0"
-OUTPUT_VERSION="1.3.0-SNAPSHOT"
-
-mvn clean install -Dstrategoxt-baseline-version=$BASELINE_VERSION -Dstrategoxt-output-version=$OUTPUT_VERSION -Dstrategoxt-output-location=target
+mvn clean install -f build-pom.xml
