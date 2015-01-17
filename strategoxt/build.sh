087b97d094b2de1d65153b9b3e4953d35d2ca867
Moritz Lichter <lichtemo@googlemail.com>
2015-01-17 22:31:45 +0100

    Increased heap for ant

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -18,4 +18,4 @@ cd ..
 : ${STRJ_BASELINE_BINS:?"Environment not initialized. Run source PLATFORM-init.sh"}
 : ${STRJ_BASELINE_BASE:?"Environment not initialized. Run source PLATFORM-init.sh"}
 
-ANT_OPTS="-Xss8m -Xmx1024m" ant -Dbaseline=$STRJ_BASELINE -Dsdf2bundle=$STRJ_BASELINE -Dnativepath=$STRJ_BASELINE_BINS -lib $STRJ_BASELINE_BASE -lib $STRJ_BASELINE_DEPS $@ || exit 1
+ANT_OPTS="-Xss8m -Xmx2048m" ant -Dbaseline=$STRJ_BASELINE -Dsdf2bundle=$STRJ_BASELINE -Dnativepath=$STRJ_BASELINE_BINS -lib $STRJ_BASELINE_BASE -lib $STRJ_BASELINE_DEPS $@ || exit 1
