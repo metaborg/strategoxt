4f2cb6ac05f9ad0f9b82097ee3203cb80f9cd08e
Moritz Lichter <lichtemo@googlemail.com>
2015-02-25 00:13:44 +0100

    Externals for strc

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/CompatLibraryInitializer.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/CompatLibraryInitializer.java
@@ -8,18 +8,26 @@ import org.strategoxt.lang.LibraryInitializer;
 import org.strategoxt.lang.RegisteringStrategy;
 import org.strategoxt.lang.compat.stratego_rtg_compat.strrtg_list_loop1_1_0;
 import org.strategoxt.lang.compat.stratego_rtg_compat.strrtg_repeat_1_0;
+import org.strategoxt.lang.compat.strc_compat.pp_c_0_0;
+import org.strategoxt.lang.compat.strc_compat.pp_c_pretty_0_0;
+import org.strategoxt.lang.compat.strc_compat.strc_cgen_aterm_0_0;
 
-public class CompatLibraryInitializer extends LibraryInitializer{
+public class CompatLibraryInitializer extends LibraryInitializer {
 
 	@Override
 	protected List<RegisteringStrategy> getLibraryStrategies() {
-		return Arrays.asList(new $Read$From$File_cached_0_0(), new report_failure_compat_1_0(), new strrtg_list_loop1_1_0(), new strrtg_repeat_1_0());
+		return Arrays.asList(
+				new $Read$From$File_cached_0_0(), 
+				new report_failure_compat_1_0(), 
+				new strrtg_list_loop1_1_0(), 
+				new strrtg_repeat_1_0(),
+				new pp_c_0_0(),
+				new pp_c_pretty_0_0(),
+				new strc_cgen_aterm_0_0());
 	}
 
 	@Override
 	protected void initializeLibrary(Context context) {
 	}
-	
-	
 
 }
