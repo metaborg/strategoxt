5fd9b4175d177b026b8bd2ef69041f8ea195c6e5
Moritz Lichter <lichtemo@googlemail.com>
2015-08-06 20:10:03 +0200

    Eliminated switch statement on String

--- strategoxt/stratego-libraries/java-backend/java/pluto-interface/org/strategoxt/strj/pluto_interface/Conversions.java
+++ strategoxt/stratego-libraries/java-backend/java/pluto-interface/org/strategoxt/strj/pluto_interface/Conversions.java
@@ -2,7 +2,6 @@ package org.strategoxt.strj.pluto_interface;
 
 import java.util.ArrayList;
 
-import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.terms.IStrategoList;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.terms.Term;
@@ -15,18 +14,19 @@ import build.pluto.output.Out;
 
 public class Conversions {
 
-	public static BuildRequest<?, Out<IStrategoTerm>, ?, ? extends BuilderFactory<?, Out<IStrategoTerm>, ?>> convertBuildRequest( IStrategoTerm term) {
-		switch (Term.tryGetName(term)) {
-		case "BuildRequest":
+	public static BuildRequest<?, Out<IStrategoTerm>, ?, ? extends BuilderFactory<?, Out<IStrategoTerm>, ?>> convertBuildRequest(
+			IStrategoTerm term) {
+		String name = Term.tryGetName(term);
+		if ("BuildRequest".equals(name)) {
 			IStrategoTerm factoryTerm = Term.termAt(term, 0);
 			IStrategoTerm input = Term.termAt(term, 1);
 
 			StrategoBuilderFactory factory = ObjectWrapperTerm.get(factoryTerm);
 
 			return new BuildRequest<>(factory, input);
-		case "BuildAtOnceBuildRequest":
-			factoryTerm = Term.termAt(term, 0);
-			input = Term.termAt(term, 1);
+		} else if ("BuildAtOnceBuildRequest".equals(name)) {
+			IStrategoTerm factoryTerm = Term.termAt(term, 0);
+			IStrategoTerm input = Term.termAt(term, 1);
 			ArrayList<IStrategoTerm> inputs;
 			if (Term.isTermList(input)) {
 				inputs = new ArrayList<>(((IStrategoList) input).size());
@@ -34,12 +34,12 @@ public class Conversions {
 					inputs.add(i);
 				}
 			} else {
-				inputs = BuildCycleAtOnceBuilder.singletonArrayList( input);
+				inputs = BuildCycleAtOnceBuilder.singletonArrayList(input);
 			}
 			StrategoBuildCycleAtOnceBuilderFactory cycleBuilderfactory = ObjectWrapperTerm.get(factoryTerm);
 			return new BuildRequest<>(cycleBuilderfactory, inputs);
 
-		default:
+		} else {
 			throw new RuntimeException("Illegal BuildRequestTerm");
 		}
 	}
