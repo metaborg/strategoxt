d10bc97747b8a4e01a4f9f4a059e8ac5a6d2d45c
Moritz Lichter <lichtemo@googlemail.com>
2015-08-17 21:54:42 +0200

    Fix for jarprovider

--- strategoxt/stratego-libraries/jarprovider/src/org/strategoxt/eclipse/ant/StrategoMinJarAntPropertyProvider.java
+++ strategoxt/stratego-libraries/jarprovider/src/org/strategoxt/eclipse/ant/StrategoMinJarAntPropertyProvider.java
@@ -5,6 +5,6 @@ import org.eclipse.ant.core.IAntPropertyValueProvider;
 public class StrategoMinJarAntPropertyProvider implements IAntPropertyValueProvider {
 
     public String getAntPropertyValue(String antPropertyName) {
-        return org.strategoxt.stratego_lib.Main.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+        return org.strategoxt.stratego_lib_generic.stratego_lib_generic.class.getProtectionDomain().getCodeSource().getLocation().getFile();
     }
 }
