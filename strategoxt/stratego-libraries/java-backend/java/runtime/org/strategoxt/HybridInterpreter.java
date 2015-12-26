236f21a797b4f82d4609e8b819d5dd0ab2e08748
Moritz Lichter <lichtemo@googlemail.com>
2015-12-26 12:53:11 +0100

    trigger buid

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/HybridInterpreter.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/HybridInterpreter.java
@@ -348,7 +348,6 @@ public class HybridInterpreter extends Interpreter implements IAsyncCancellable 
 	public void loadJars(ClassLoader parentClassLoader, URL... jars)
 			throws SecurityException, NoInteropRegistererJarException, IncompatibleJarException, IOException {
 
-		
 		URLClassLoader classLoader = new URLClassLoader(jars, parentClassLoader);
 		boolean foundRegisterer = false;
 		loadedJars = true;
