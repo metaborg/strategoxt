578fa66cd5c9cb81bd6f16bffb395e3c9822170d
Hendrik van Antwerpen <hendrik@van-antwerpen.net>
2017-02-10 16:49:30 +0100

    Restore stack depth after catching an exception.

--- strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/SSL_EXT_catch_with_0_2.java
+++ strategoxt/stratego-libraries/java-backend/java/runtime/org/strategoxt/lang/compat/SSL_EXT_catch_with_0_2.java
@@ -5,8 +5,11 @@ package org.strategoxt.lang.compat;
 
 import org.spoofax.interpreter.core.IContext;
 import org.spoofax.interpreter.core.InterpreterException;
+import org.spoofax.interpreter.core.StackTracer.TryState;
 import org.spoofax.interpreter.library.AbstractPrimitive;
 import org.spoofax.interpreter.stratego.Strategy;
+import org.spoofax.interpreter.terms.IStrategoList;
+import org.spoofax.interpreter.terms.IStrategoString;
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.spoofax.interpreter.terms.ITermFactory;
 import org.strategoxt.lang.StrategoErrorExit;
@@ -33,26 +36,23 @@ public class SSL_EXT_catch_with_0_2 extends AbstractPrimitive {
 							+ svars.length);
 		}
 
-		IStrategoTerm ct = env.current();
-		ITermFactory factory = env.getFactory();
-
+		final IStrategoTerm current = env.current();
+		final ITermFactory factory = env.getFactory();
+		final TryState tryState = env.getStackTracer().pushTry();
+	
 		try {
 			return svars[0].evaluate(env);
 		} catch (StrategoErrorExit e) {
-			for(String s : env.getStackTracer().getTrace()) {
-				System.err.println(s);
+		    tryState.caught();
+		    final IStrategoString message = factory.makeString(e.getMessage() != null ? e.getMessage() : "");
+		    final IStrategoList trace = e.getTrace() != null ? e.getTrace() : factory.makeList();
+		    final IStrategoTerm exception = factory.makeTuple(new IStrategoTerm[]{ message, current, trace }, factory.makeList());
+			env.setCurrent(exception);
+			boolean success = svars[1].evaluate(env);
+			if (!success) {
+				env.setCurrent(current);
 			}
-			IStrategoTerm nt = null;
-			if(e.getTrace() != null)
-				nt = factory.makeTuple(new IStrategoTerm[]{ct, e.getTrace()}, factory.makeList());
-			else
-				nt = factory.makeTuple(new IStrategoTerm[]{ct, factory.makeList()}, factory.makeList());
-			env.setCurrent(nt);
-			boolean s2 = svars[1].evaluate(env);
-			if (!s2) {
-				env.setCurrent(ct);
-			}
-			return s2;
+			return success;
 		}
 	}
 
