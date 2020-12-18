0b3f03605563b5b7f7d268f865753f3b50279cf4
Gabriel Konat <gabrielkonat@gmail.com>
2020-12-18 15:55:28 +0100

    Add Gradle build files.

---
+++ strategoxt/stratego-libraries/java-backend/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+}
+
+sourceSets {
+  main {
+    java {
+      srcDir("java/runtime")
+    }
+  }
+}
+
+fun compositeBuild(name: String) = "$group:$name:$version"
+val spoofax2Version: String by ext
+dependencies {
+  api(platform("org.metaborg:parent:$spoofax2Version"))
+
+  api(compositeBuild("org.spoofax.terms"))
+  api(compositeBuild("org.spoofax.interpreter.core"))
+  api(compositeBuild("org.spoofax.interpreter.library.xml"))
+  api(compositeBuild("org.spoofax.interpreter.library.java"))
+  api(compositeBuild("org.spoofax.interpreter.library.index"))
+  api(compositeBuild("org.spoofax.jsglr"))
+  api(compositeBuild("org.spoofax.interpreter.library.jsglr"))
+  api("org.metaborg:strategoxt-min-jar:$spoofax2Version")
+
+  compileOnly("com.google.code.findbugs:jsr305")
+}
+
+// TODO: copy the StrategoXT runnable JAR into project, which is exported as a resource? We probably do not need it in
+// devenv/Spoofax 3, as we do not use the embedded StrategoXT JAR.
