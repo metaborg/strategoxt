2169aa154db80c31d7f5f549c78b7109317e5678
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-27 13:46:03 +0200

    Reformat code to match Kotlin official style

--- strategoxt/stratego-libraries/java-backend/build.gradle.kts
+++ strategoxt/stratego-libraries/java-backend/build.gradle.kts
@@ -1,30 +1,30 @@
 plugins {
-  id("org.metaborg.gradle.config.java-library")
+    id("org.metaborg.gradle.config.java-library")
 }
 
 sourceSets {
-  main {
-    java {
-      srcDir("java/runtime")
+    main {
+        java {
+            srcDir("java/runtime")
+        }
     }
-  }
 }
 
 fun compositeBuild(name: String) = "$group:$name:$version"
 val spoofax2Version: String by ext
 dependencies {
-  api(platform("org.metaborg:parent:$spoofax2Version"))
+    api(platform("org.metaborg:parent:$spoofax2Version"))
 
-  api(compositeBuild("org.spoofax.terms"))
-  api(compositeBuild("org.spoofax.interpreter.core"))
-  api(compositeBuild("org.spoofax.interpreter.library.xml"))
-  api(compositeBuild("org.spoofax.interpreter.library.java"))
-  api(compositeBuild("org.spoofax.interpreter.library.index"))
-  api(compositeBuild("org.spoofax.jsglr"))
-  api(compositeBuild("org.spoofax.interpreter.library.jsglr"))
-  api("org.metaborg:strategoxt-min-jar:$spoofax2Version")
+    api(compositeBuild("org.spoofax.terms"))
+    api(compositeBuild("org.spoofax.interpreter.core"))
+    api(compositeBuild("org.spoofax.interpreter.library.xml"))
+    api(compositeBuild("org.spoofax.interpreter.library.java"))
+    api(compositeBuild("org.spoofax.interpreter.library.index"))
+    api(compositeBuild("org.spoofax.jsglr"))
+    api(compositeBuild("org.spoofax.interpreter.library.jsglr"))
+    api("org.metaborg:strategoxt-min-jar:$spoofax2Version")
 
-  implementation("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 }
 
 // TODO: copy the StrategoXT runnable JAR into project, which is exported as a resource? We probably do not need it in
